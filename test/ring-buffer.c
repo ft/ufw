3dbec29f22e4a02e369c3c3e551de74c5776575e
Frank Terbeck <ft@bewatermyfriend.org>
2017-09-25 22:39:23 +0200

    tests: Build ring-buffer test suite
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- test/ring-buffer.c
+++ test/ring-buffer.c
@@ -8,6 +8,8 @@
 #include <c/ring-buffer-iter.c>
 #include <compiler.h>
 
+#include <tap.h>
+
 RING_BUFFER_API(byte_buffer, uint8_t)
 RING_BUFFER_ITER_API(byte_buffer, uint8_t)
 
@@ -18,25 +20,30 @@ int
 main(UNUSED int argc, UNUSED char *argv[])
 {
     const size_t bufsize = 16;
+    const uint8_t data[] = { 11, 22, 33, 44, 55, 66, 77, 88 };
     uint8_t buffer[bufsize];
     byte_buffer foo;
     rb_iter iter;
 
+    plan(16);
+
     /* Initialise ring-buffer with buffer as its data-storage. */
     byte_buffer_init(&foo, buffer, bufsize);
 
-    /* Put two bytes into the buffer. */
-    byte_buffer_put(&foo, 23);
-    byte_buffer_put(&foo, 42);
+    /* Put data into the buffer. */
+    for (size_t i = 0; i < sizeof(data); ++i)
+        byte_buffer_put(&foo, data[i]);
 
     /* Iterate across the buffer; this consumes nothing. */
     byte_buffer_iter(&iter, &foo, RING_BUFFER_ITER_OLD_TO_NEW);
     for (size_t i = 0; !rb_iter_done(&iter); rb_iter_advance(&iter), i++)
-        printf("# %lu: %u (iterate)\n", i, byte_buffer_inspect(&foo, &iter));
+        cmp_ok(byte_buffer_inspect(&foo, &iter), "==", data[i],
+               "(iterate) data[%lu] found", i);
 
     /* Consume data until the buffer is empty. */
     for (size_t i = 0; !byte_buffer_empty(&foo); ++i)
-        printf("# %lu: %u (fetch)\n", i, byte_buffer_get(&foo));
+        cmp_ok(byte_buffer_get(&foo), "==", data[i],
+               "(get) data[%lu] found", i);
 
     return EXIT_SUCCESS;
 }
