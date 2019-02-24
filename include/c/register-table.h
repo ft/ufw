415af8af2bdf2517c3d33a7aee029818ac69b2e0
Frank Terbeck <ft@bewatermyfriend.org>
2019-02-24 21:35:12 +0100

    Add first version of a register table abstraction
    The idea here is to allow the definition of an external interface for an
    embedded system as a chunk of linear memory. The application side should be
    able to trivially interact with the contents of that table. On the other side
    of the picture the communication system of the system should have functions to
    easily read and write blocks of register data.
    
    Another driving idea behind this implementation is to be able to express an
    entire, even complex, register table in one C language expression in order to
    keep things tidy.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ include/c/register-table.h
@@ -0,0 +1,124 @@
+#ifndef INC_REGISTER_TABLE_H
+#define INC_REGISTER_TABLE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/* Data types */
+
+typedef uint16_t RegisterAtom;
+typedef size_t RegisterAddress;
+typedef size_t RegisterOffset;
+typedef struct RegisterArea RegisterArea;
+typedef struct RegisterValue RegisterValue;
+
+typedef enum RegisterAccessBits {
+    REG_ACCESS_BIT_READ  = (1u << 0u),
+    REG_ACCESS_BIT_WRITE = (1u << 1u)
+} RegisterAccessBits;
+
+typedef enum RegisterAccessCode {
+    REG_ACCESS_SUCCESS,
+    REG_ACCESS_NOENTRY,
+    REG_ACCESS_INVALID,
+    REG_ACCESS_READONLY
+} RegisterAccessCode;
+
+typedef struct RegisterAccessResult {
+    RegisterAccessCode code;
+    RegisterAddress address;
+} RegisterAccessResult;
+
+#define REG_ACCESS_RESULT_INIT { REG_ACCESS_SUCCESS, 0u }
+
+typedef bool(*registerSer)(const RegisterValue*, RegisterAtom*);
+typedef bool(*registerDes)(const RegisterAtom*, RegisterValue*);
+
+typedef RegisterAccessResult(*registerRead)(
+    const RegisterArea*, RegisterAtom*, RegisterOffset, size_t);
+typedef RegisterAccessResult(*registerWrite)(
+    RegisterArea*, const RegisterAtom*, RegisterOffset, size_t);
+
+typedef struct RegisterString {
+    char *data;
+    size_t size;
+} RegisterString;
+
+typedef enum RegisterType {
+    REG_TYPE_INVALID = 0u,
+    REG_TYPE_UINT16,
+    REG_TYPE_UINT32,
+    REG_TYPE_UINT64,
+    REG_TYPE_SINT16,
+    REG_TYPE_SINT32,
+    REG_TYPE_SINT64,
+    REG_TYPE_FLOAT32,
+    REG_TYPE_STRING
+} RegisterType;
+
+#define REG_ATOM_BIGGEST_DATUM (sizeof(uint64_t) / sizeof(RegisterAtom))
+
+typedef union RegisterValueU {
+    uint16_t u16;
+    uint32_t u32;
+    uint64_t u64;
+    int16_t s16;
+    int32_t s32;
+    int64_t s64;
+    float f32;
+    RegisterString string;
+} RegisterValueU;
+
+struct RegisterValue {
+    RegisterType type;
+    RegisterValueU value;
+};
+
+typedef struct RegisterSerDes {
+    registerSer ser;
+    registerDes des;
+    size_t size;
+} RegisterSerDes;
+
+typedef struct RegisterEntry {
+    RegisterType type;
+    RegisterValueU default_value;
+    RegisterAddress address;
+    RegisterArea *area;
+    RegisterOffset offset;
+} RegisterEntry;
+
+#define REG_ENTRY_END                                   \
+    { .type = REG_TYPE_INVALID, .default_value.u16 = 0, \
+      .address = 0, .area = NULL, .offset = 0 }
+
+struct RegisterArea {
+    registerRead read;
+    registerWrite write;
+    size_t base;
+    size_t size;
+    RegisterAtom *mem;
+};
+
+#define REG_MEMORY_END \
+    { .read = NULL, .write = NULL, .base = 0, .size = 0, .mem = NULL }
+
+typedef struct RegisterTable {
+    size_t areas;
+    RegisterArea *area;
+    size_t entries;
+    RegisterEntry *entry;
+} RegisterTable;
+
+/* Public API prototypes */
+
+RegisterAccessResult
+reg_mem_read(const RegisterArea*, RegisterAtom*, RegisterOffset, size_t);
+RegisterAccessResult
+reg_mem_write(RegisterArea*, const RegisterAtom*, RegisterOffset, size_t);
+
+RegisterAccessResult register_set(RegisterTable*, size_t, RegisterValue*);
+RegisterAccessResult register_get(RegisterTable*, size_t, RegisterValue*);
+
+#endif /* INC_REGISTER_TABLE_H */
---
+++ src/c/register-table.c
@@ -0,0 +1,248 @@
+#ifndef DEBUG
+#define NDEBUG
+#endif /* DEBUG */
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef INC_REGISTER_TABLE_H
+#include <c/register-table.h>
+#endif /* INC_REGISTER_TABLE_H */
+
+/* Internal API */
+
+#define regmem_read(rm, dst, o, n) ((rm).read(&(rm), (dst), (o), (n)))
+#define regmem_write(rm, src, o, n) ((rm).write(&(rm), (src), (o), (n)))
+
+static bool
+rds_invalid_ser(const RegisterValue *v, RegisterAtom *r)
+{
+    (void)v;
+    (void)r;
+    assert(false);
+    return false;
+}
+
+static bool
+rds_invalid_des(const RegisterAtom *r, RegisterValue *v)
+{
+    (void)r;
+    (void)v;
+    assert(false);
+    return false;
+}
+
+static bool
+rds_u16_ser(const RegisterValue *v, RegisterAtom *r)
+{
+    assert(v->type == REG_TYPE_UINT16);
+    *r = v->value.u16;
+    return true;
+}
+
+static bool
+rds_u16_des(const RegisterAtom *r, RegisterValue *v)
+{
+    uint16_t n = *r;
+    v->value.u16 = n;
+    v->type = REG_TYPE_UINT16;
+    return true;
+}
+
+#define rs(t) (sizeof(t) / sizeof(RegisterAtom))
+
+const RegisterSerDes rds_serdes[] = {
+    [REG_TYPE_INVALID] = { rds_invalid_ser, rds_invalid_des, 0 },
+    [REG_TYPE_UINT16] = { rds_u16_ser, rds_u16_des, rs(uint16_t) }
+};
+
+static inline bool
+is_end_of_areas(RegisterArea *a)
+{
+    if (a->read != NULL || a->write != NULL)
+        return false;
+    if (a->size != 0 || a->base != 0)
+        return false;
+    if (a->mem != NULL)
+        return false;
+    return true;
+}
+
+static size_t
+reg_count_areas(RegisterArea *a)
+{
+    size_t n = 0ull;
+    while (is_end_of_areas(a) == false) {
+        n++;
+        a++;
+    }
+    return n;
+}
+
+static inline bool
+is_end_of_entries(RegisterEntry *e)
+{
+    return (e->type == REG_TYPE_INVALID);
+}
+
+static size_t
+reg_count_entries(RegisterEntry *e)
+{
+    size_t n = 0ull;
+    while (is_end_of_entries(e) == false) {
+        n++;
+        e++;
+    }
+    return n;
+}
+
+static bool
+ra_reg_is_part_of(RegisterArea *a, RegisterEntry *e)
+{
+    if (a->base > e->address)
+        return false;
+
+    if ((a->base + a->size) <= e->address)
+        return false;
+
+    return true;
+}
+
+static bool
+ra_reg_fits_into(RegisterArea *a, RegisterEntry *e)
+{
+    const size_t area_end = a->base + a->size;
+    const size_t entry_end = e->address + rds_serdes[e->type].size;
+    return (area_end <= entry_end);
+}
+
+static bool
+ra_find_memory(RegisterTable *t, RegisterEntry *e)
+{
+    for (size_t an = 0ull; an < t->areas; ++an) {
+        RegisterArea *area = &t->area[an];
+        if (ra_reg_is_part_of(area, e)) {
+            if (ra_reg_fits_into(area, e))
+                return false;
+
+            e->area = area;
+            e->offset = e->address - area->base;
+            return true;
+        }
+    }
+    return false;
+}
+
+/* Public API */
+
+RegisterAccessResult
+registers_init(RegisterTable *t)
+{
+    RegisterAtom raw[REG_ATOM_BIGGEST_DATUM];
+    RegisterAccessResult rv = REG_ACCESS_RESULT_INIT;
+
+    /* Determine table sizes first */
+    t->areas = reg_count_areas(t->area);
+    t->entries = reg_count_entries(t->entry);
+
+    for (size_t i = 0ull; i < t->entries; ++i) {
+        RegisterValue def;
+        RegisterEntry *e = &t->entry[i];
+        /* Link into register table memory */
+        bool success = ra_find_memory(t, e);
+        if (success == false) {
+            rv.code = REG_ACCESS_NOENTRY;
+            rv.address = e->address;
+            return rv;
+        }
+        /* Load default value into register table */
+        def.value = e->default_value;
+        def.type = e->type;
+        success = rds_serdes[e->type].ser(&def, raw);
+
+        if (success == false) {
+            rv.code = REG_ACCESS_INVALID;
+            rv.address = e->address;
+            return rv;
+        }
+        e->area->write(e->area, raw, e->offset, rds_serdes[e->type].size);
+    }
+
+    return rv;
+}
+
+RegisterAccessResult
+register_set(RegisterTable *t, size_t idx, RegisterValue *v)
+{
+    RegisterAtom raw[REG_ATOM_BIGGEST_DATUM];
+    RegisterAccessResult rv = REG_ACCESS_RESULT_INIT;
+    RegisterEntry *e;
+    RegisterArea *a;
+    bool success;
+
+    if (idx > t->entries) {
+        rv.code = REG_ACCESS_NOENTRY;
+        rv.address = idx;
+        return rv;
+    }
+
+    e = &t->entry[idx];
+    a = e->area;
+    success = rds_serdes[e->type].ser(v, raw);
+
+    if (success == false) {
+        rv.code = REG_ACCESS_INVALID;
+        rv.address = e->address;
+        return rv;
+    }
+
+    a->write(a, raw, e->offset, rds_serdes[e->type].size);
+    return rv;
+}
+
+RegisterAccessResult
+register_get(RegisterTable *t, size_t idx, RegisterValue *v)
+{
+    RegisterAtom raw[REG_ATOM_BIGGEST_DATUM];
+    RegisterAccessResult rv = REG_ACCESS_RESULT_INIT;
+    RegisterEntry *e;
+    RegisterArea *a;
+    bool success;
+
+    if (idx > t->entries) {
+        rv.code = REG_ACCESS_NOENTRY;
+        rv.address = idx;
+        return rv;
+    }
+
+    e = &t->entry[idx];
+    a = e->area;
+    a->read(a, raw, e->offset, rds_serdes[e->type].size);
+    success = rds_serdes[e->type].des(raw, v);
+
+    if (success == false) {
+        rv.code = REG_ACCESS_INVALID;
+        rv.address = idx;
+    }
+    return rv;
+}
+
+RegisterAccessResult
+reg_mem_read(const RegisterArea *a, RegisterAtom *dest,
+             RegisterOffset offset, size_t n)
+{
+    RegisterAccessResult rv = REG_ACCESS_RESULT_INIT;
+    memcpy(dest, a->mem + offset, n * sizeof(RegisterAtom));
+    return rv;
+}
+
+RegisterAccessResult
+reg_mem_write(RegisterArea *a, const RegisterAtom *src,
+              RegisterOffset offset, size_t n)
+{
+    RegisterAccessResult rv = REG_ACCESS_RESULT_INIT;
+    memcpy(a->mem + offset, src, n * sizeof(RegisterAtom));
+    return rv;
+}
