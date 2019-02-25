5a30ca56764b8793945c711a457b7a30988fa481
Frank Terbeck <ft@bewatermyfriend.org>
2019-02-25 01:44:39 +0100

    Mark string ser/des as invalid for now
    Since strings do not have a fixed size, it's not quite as easy to fit them into
    this frame-work.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- src/c/register-table.c
+++ src/c/register-table.c
@@ -189,7 +189,8 @@ const RegisterSerDes rds_serdes[] = {
     [REG_TYPE_SINT16] = { rds_s16_ser, rds_s16_des, rs(int16_t) },
     [REG_TYPE_SINT32] = { rds_s32_ser, rds_s32_des, rs(int32_t) },
     [REG_TYPE_SINT64] = { rds_s64_ser, rds_s64_des, rs(int64_t) },
-    [REG_TYPE_FLOAT32] = { rds_f32_ser, rds_f32_des, rs(float) }
+    [REG_TYPE_FLOAT32] = { rds_f32_ser, rds_f32_des, rs(float) },
+    [REG_TYPE_STRING] = { rds_invalid_ser, rds_invalid_des, 0 }
 };
 
 static inline bool
