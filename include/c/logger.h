31fdbdc08d87db9e146da45c3a0f5854243612e0
Iqbal Maraqa <iqbal_maraqa@hotmail.com>
2019-03-26 11:48:26 +0100

    Add parts of a logging framework
    Signed-off-by: Iqbal Maraqa <iqbal_maraqa@hotmail.com>

---
+++ include/c/logger.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2019 ufw workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+/**
+ * @file logger.h
+ * @brief This files contains datatype definitions and function prototypes.
+ */
+
+#ifndef __DEBUG_UART_H
+#define __DEBUG_UART_H
+
+/**
+ * @addtogroup ufw
+ * @{
+ * @addtogroup Logger
+ * @{
+ * @addtogroup Datatypes
+ * @{
+ */
+
+/** Defines an enum datatype for colours */
+typedef enum {
+    /** Default colour */
+    LOG_DEFAULT_COLOUR = 0,
+    /** Red, foreground */
+    LOG_RED_FG = 31,
+    /** Red, background */
+    LOG_RED_BG = 41,
+    /** Green, foreground */
+    LOG_GREEN_FG = 32,
+    /** Yellow, foreground */
+    LOG_YELLOW_FG = 33,
+    /** Blue, foreground */
+    LOG_BLUE_FG = 34,
+    /** White, foreground */
+    LOG_WHITE_FG = 37,
+    /** Bright blue, foreground */
+    LOG_BLUE_BRIGHT_FG = 94
+} LogColour;
+
+/**
+ * Defines an enum datatype for the verbosity levels.
+ *
+ * The verbosity level is interpreted as a bit mask.
+ */
+typedef enum {
+    /** "DEBUG" will be printed in blue */
+    LOG_DEBUG = 1u << 0,
+    /** "INFO" will be printed in green */
+    LOG_INFO = 1u << 1,
+    /** "WARNING" will be printed in yellow */
+    LOG_WARNING = 1u << 2,
+    /** "ERROR" will be printed in red */
+    LOG_ERROR = 1u << 3,
+    /** "FATAL" will be printed in white on red */
+    LOG_FATAL = 1u << 4
+} LogLevel;
+
+/**
+ * @}
+ *
+ * @addtogroup Functions
+ * @{
+ */
+
+void log_set_level(LogLevel level);
+void log_print(LogLevel level, const char *msg);
+void log_printf(LogLevel level, const char *frmt_msg, ...);
+
+/**
+ * @}
+ * @}
+ * @}
+ */
+
+#endif /* __DEBUG_UART_H */
