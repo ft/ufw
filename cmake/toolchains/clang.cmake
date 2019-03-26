ab49899e40d6be5ced9a00a86e05c645ee3f95de
Frank Terbeck <ft@bewatermyfriend.org>
2019-03-26 14:38:06 +0100

    Add explicit toolchain-files for gnu and clang
    …even when building natively.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ cmake/toolchains/clang.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR native)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+
+set(TOOLCHAIN_ID "clang-native")
+set(COMPILER_API "gnu")
---
+++ cmake/toolchains/gnu.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR native)
+
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+set(TOOLCHAIN_ID "gcc-native")
+set(COMPILER_API "gnu")
