29b202f2414a583da05f56ef9ddab38f8e17cc74
Frank Terbeck <ft@bewatermyfriend.org>
2019-03-29 16:22:51 +0100

    Run area initialisation tests
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- test/t-register-table.c
+++ test/t-register-table.c
@@ -65,11 +65,60 @@ t_trivial_fail(void)
     cmp_ok(success.code, "==", REG_INIT_NO_AREAS, "Trivial table fails");
 }
 
+static void
+t_area_init_checks(void)
+{
+    /* A register table needs at least one area. */
+    RegisterTable r_overlap = {
+        .area = (RegisterArea[]) {
+            MEMORY_AREA(0x0000ul, 0x10ul),
+            MEMORY_AREA(0x000ful, 0x10ul),
+            REGISTER_AREA_END
+        },
+        .entry = (RegisterEntry[]) {
+            REGISTER_ENTRY_END
+        }
+    };
+    RegisterTable r_just_no_overlap = {
+        .area = (RegisterArea[]) {
+            MEMORY_AREA(0x0000ul, 0x10ul),
+            MEMORY_AREA(0x0010ul, 0x10ul),
+            REGISTER_AREA_END
+        },
+        .entry = (RegisterEntry[]) {
+            REGISTER_ENTRY_END
+        }
+    };
+    RegisterTable r_area_order = {
+        .area = (RegisterArea[]) {
+            MEMORY_AREA(0x0010ul, 0x10ul),
+            MEMORY_AREA(0x0000ul, 0x10ul),
+            REGISTER_AREA_END
+        },
+        .entry = (RegisterEntry[]) {
+            REGISTER_ENTRY_END
+        }
+    };
+
+    RegisterInit success = register_init(&r_overlap);
+    cmp_ok(success.code, "==", REG_INIT_AREA_ADDRESS_OVERLAP,
+           "Area overlap detected");
+    success = register_init(&r_just_no_overlap);
+    cmp_ok(success.code, "==", REG_INIT_SUCCESS,
+           "Adjacent areas without gap in between succeeds");
+    success = register_init(&r_area_order);
+    cmp_ok(success.code, "==", REG_INIT_AREA_INVALID_ORDER,
+           "Area order is not proper");
+    cmp_ok(success.pos.area, "==", 1,
+           "  ...area at index 1 yielded the error");
+}
+
 int
 main(UNUSED int argc, UNUSED char *argv[])
 {
-    plan(5);
-    t_invalid_tables();   /* 3 */
-    t_trivial_success();  /* 1 */
-    t_trivial_fail();     /* 1 */
+    plan(9);
+    t_invalid_tables();    /* 3 */
+    t_trivial_success();   /* 1 */
+    t_trivial_fail();      /* 1 */
+    t_area_init_checks();  /* 4 */
 }
