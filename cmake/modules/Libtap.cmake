92b64d20d91f9d92c3d92c5da0257fd988d29a6f
Frank Terbeck <ft@bewatermyfriend.org>
2019-05-27 18:31:46 +0200

    Only try building libtap with native targets
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- cmake/modules/Libtap.cmake
+++ cmake/modules/Libtap.cmake
@@ -4,18 +4,22 @@ endif()
 set(__UFW_Libtap 1)
 
 macro(add_libtap libtap_root)
-  include(InitialiseToolchain)
-  initialise_toolchain()
+  if (PROJECT_TARGET_CPU AND (${PROJECT_TARGET_CPU} STREQUAL "native"))
+    include(InitialiseToolchain)
+    initialise_toolchain()
 
-  add_library(tap STATIC ${libtap_root}/tap.c)
-  target_include_directories(tap PUBLIC ${libtap_root})
+    add_library(tap STATIC ${libtap_root}/tap.c)
+    target_include_directories(tap PUBLIC ${libtap_root})
 
-  # There are a couple of warnings in tap.c (mainly about unused parameters).
-  # I won't bother fixing them in third-party software and since there is no
-  # point in being distracted by them, I am not enabling strict warnings in
-  # this code.
-  #MakeStrictCompilerC(tap)
+    # There are a couple of warnings in tap.c (mainly about unused parameters).
+    # I won't bother fixing them in third-party software and since there is no
+    # point in being distracted by them, I am not enabling strict warnings in
+    # this code.
+    #MakeStrictCompilerC(tap)
 
-  include(SetupTargetCPU)
-  set_target_cpu(tap)
+    include(SetupTargetCPU)
+    set_target_cpu(tap)
+  else()
+    message(STATUS "The libtap library is only built on native targets")
+  endif()
 endmacro()
