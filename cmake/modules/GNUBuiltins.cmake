98b906d3e3c94ad221c4989eddcee01a04f79d26
Frank Terbeck <ft@bewatermyfriend.org>
2019-10-04 00:39:01 +0200

    Add tests for __builtin_bswapNN() compiler functions
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- cmake/modules/GNUBuiltins.cmake
+++ cmake/modules/GNUBuiltins.cmake
@@ -19,15 +19,41 @@ macro(CheckGNUBuiltin_C_expect)
     UFW_CC_HAS_BUILTIN_EXPECT)
 endmacro()
 
-macro(CheckAllGNUBuiltins_C)
-  CheckGNUBuiltin_C_expect()
-endmacro()
-
 macro(CheckGNUBuiltin_CXX_expect)
   check_cxx_source_compiles("${__UFW_GNUBuiltin_expect}"
     UFW_CXX_HAS_BUILTIN_EXPECT)
 endmacro()
 
+function(CheckGNUBuiltin_bswap_n compiler width)
+  set(__code
+    "int main(void) {
+       __builtin_bswap${width}(0);
+       return 0;
+     }")
+  set(_result "UFW_${compiler}_HAS_BUILTIN_BSWAP${width}")
+  set(result ${_result})
+  check_c_source_compiles("${__UFW_GNUBuiltin_expect}" ${_result})
+  set(${result} ${_result} PARENT_SCOPE)
+endfunction()
+
+macro(CheckGNUBuiltin_C_bswap)
+  CheckGNUBuiltin_bswap_n(CC 16)
+  CheckGNUBuiltin_bswap_n(CC 32)
+  CheckGNUBuiltin_bswap_n(CC 64)
+endmacro()
+
+macro(CheckGNUBuiltin_CXX_bswap)
+  CheckGNUBuiltin_bswap_n(CXX 16)
+  CheckGNUBuiltin_bswap_n(CXX 32)
+  CheckGNUBuiltin_bswap_n(CXX 64)
+endmacro()
+
+macro(CheckAllGNUBuiltins_C)
+  CheckGNUBuiltin_C_expect()
+  CheckGNUBuiltin_C_bswap()
+endmacro()
+
 macro(CheckAllGNUBuiltins_CXX)
   CheckGNUBuiltin_CXX_expect()
+  CheckGNUBuiltin_CXX_bswap()
 endmacro()
