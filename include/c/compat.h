9796db0edc443048dc9974430077b71bea1c6ec6
Frank Terbeck <ft@bewatermyfriend.org>
2019-10-08 15:46:38 +0200

    Add useful compatibility code
    This adds strlcat, strlcpy and strnlen from OpenBSD. And this tries to work
    around toolchains that don't have sys/types.h and defines ssize_t if it can.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ include/c/compat.h
@@ -0,0 +1,52 @@
+#ifndef INC_UFW_COMPAT_H
+#define INC_UFW_COMPAT_H
+
+#include <string.h>
+
+#include <common/toolchain.h>
+
+#ifdef WITH_SYS_TYPES_H
+#include <sys/types.h>
+
+#else
+
+#ifndef _SSIZE_T_DECLARED
+
+#include <stdint.h>
+#include <stddef.h>
+#include <limits.h>
+
+#ifndef SIZE_MAX
+#warning "No value for SIZE_MAX found!"
+#endif
+
+#if SIZE_MAX == UINT_MAX
+typedef int ssize_t;
+#define _SSIZE_T_DECLARED
+
+#elif SIZE_MAX == ULONG_MAX
+typedef long int ssize_t;
+#define _SSIZE_T_DECLARED
+
+#else
+#error "How big is ssize_t?"
+
+#endif /* SIZE_MAX */
+
+#endif /* !_SSIZE_T_DECLARED */
+
+#endif /* WITH_SYS_TYPES_H */
+
+#ifndef UFW_COMPAT_HAVE_STRLCAT
+size_t strlcat(char *, const char *, size_t);
+#endif /* !UFW_COMPAT_HAVE_STRLCAT */
+
+#ifndef UFW_COMPAT_HAVE_STRLCPY
+size_t strlcpy(char *, const char *, size_t);
+#endif /* !UFW_COMPAT_HAVE_STRLCPY */
+
+#ifndef UFW_COMPAT_HAVE_STRNLEN
+size_t strnlen(const char *, size_t);
+#endif /* !UFW_COMPAT_HAVE_STRNLEN */
+
+#endif /* INC_UFW_COMPAT_H */
