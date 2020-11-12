81d26d881e488f82fd2dc0894baa9d5574a56e90
Frank Terbeck <ft@bewatermyfriend.org>
2020-11-12 15:33:05 +0100

    Extend toolchain feature for native clang toolchain
    This adds coverage configuration, integer sanitizer and fuzzer library support.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- cmake/toolchains/clang.cmake
+++ cmake/toolchains/clang.cmake
@@ -7,12 +7,36 @@ set(CMAKE_CXX_COMPILER clang++)
 set(TOOLCHAIN_ID "clang-native")
 set(COMPILER_API "gnu")
 
-set(TOOLCHAIN_FEATURES sanitize-address sanitize-undefined-behaviour)
+set(TOOLCHAIN_FEATURES
+  sanitize-address
+  sanitize-coverage-thorough
+  sanitize-integer
+  sanitize-undefined-behaviour
+  fuzzer-libfuzzer)
 
 set(TOOLCHAIN_FEATURE_SANITIZE_ADDRESS
   COMPILER_OPTIONS -fsanitize=address
   LINKER_OPTIONS   -fsanitize=address)
 
+set(TOOLCHAIN_FEATURE_SANITIZE_COVERAGE_FAST
+  COMPILER_OPTIONS -fsanitize-coverage=func
+  LINKER_OPTIONS   -fsanitize-coverage=func)
+
+set(TOOLCHAIN_FEATURE_SANITIZE_COVERAGE_BASIC
+  COMPILER_OPTIONS -fsanitize-coverage=bb
+  LINKER_OPTIONS   -fsanitize-coverage=bb)
+
+set(TOOLCHAIN_FEATURE_SANITIZE_COVERAGE_THOROUGH
+  COMPILER_OPTIONS -fsanitize-coverage=edge
+  LINKER_OPTIONS   -fsanitize-coverage=edge)
+
+set(TOOLCHAIN_FEATURE_SANITIZE_INTEGER
+  COMPILER_OPTIONS -fsanitize=integer
+  LINKER_OPTIONS   -fsanitize=integer)
+
 set(TOOLCHAIN_FEATURE_SANITIZE_UNDEFINED_BEHAVIOUR
   COMPILER_OPTIONS -fsanitize=undefined
   LINKER_OPTIONS   -fsanitize=undefined)
+
+set(TOOLCHAIN_FEATURE_FUZZER_LIBFUZZER
+  LINKER_OPTIONS -lFuzzer)
