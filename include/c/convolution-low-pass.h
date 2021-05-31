6e03c7ddea74e90d9996ee36cfe4fe437965968e
Martin Gritzan <martin.gritzan@gmail.com>
2021-05-31 15:19:07 +0200

    Add convolution low pass filter functions.
    Allows computing moving averages and median filter over arbitrary length
    and type. Optional API for median filter because it requires stdlib for
    qsort.
    
    Signed-off-by: Martin Gritzan <martin.gritzan@gmail.com>

---
+++ include/c/convolution-low-pass.h
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2021 micro framework workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+#ifndef INC_CONVOLUTION_LOW_PASS_H
+#define INC_CONVOLUTION_LOW_PASS_H
+
+/**
+ * @addtogroup ufw
+ * @{
+ * @addtogroup convolution low pass filters
+ * @{
+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Implement sliding-window convolution filters.
+ *
+ * The operations +, =, /, must be implemented for the desired TYPE.
+ * Also, the sum of the all elements must be ensured to never exceed TYPE's
+ * range limitations.
+ */
+#define CONV_LOW_PASS(NAME, TYPE)                                             \
+    CONV_LOW_PASS_INIT__(NAME, TYPE)                                          \
+    CONV_LOW_PASS_UPDATE__(NAME, TYPE)
+
+#define CONV_LOW_PASS_API(NAME, TYPE)                                         \
+    CONV_LOW_PASS_TYPE__(NAME, TYPE)                                          \
+    CONV_LOW_PASS_INIT_API__(NAME, TYPE)                                      \
+    CONV_LOW_PASS_UPDATE_API__(NAME, TYPE)                                    \
+    CONV_LOW_PASS_GETTERS__(NAME, TYPE)
+
+#define CONV_LOW_PASS_TYPE__(NAME, TYPE)                                      \
+    typedef struct {                                                          \
+        TYPE *win;                                                            \
+        unsigned int len;                                                     \
+        TYPE avg;                                                             \
+        bool first;                                                           \
+        unsigned int cur;                                                     \
+    } NAME;
+
+#define CONV_LOW_PASS_INIT_API__(NAME, TYPE) \
+    void NAME##_init(NAME *, TYPE *, unsigned int);
+
+#define CONV_LOW_PASS_INIT__(NAME, TYPE)                                      \
+    void NAME##_init(NAME *w, TYPE *buffer, unsigned int len) {               \
+        w->win = buffer;                                                      \
+        w->len = len;                                                         \
+        for (unsigned int i = 0; i < w->len; ++i)                             \
+            w->win[i] = (TYPE)0;                                              \
+        w->first = true;                                                      \
+        w->cur = 0;                                                           \
+        w->avg = (TYPE)0;                                                     \
+    }
+
+#define CONV_LOW_PASS_GETTERS__(NAME, TYPE)                                   \
+    inline static TYPE NAME##_avg(NAME *w) {                                  \
+        return w->avg;                                                        \
+    }                                                                         \
+                                                                              \
+    inline static bool NAME##_has_min_values(NAME *w, unsigned int c) {       \
+        if (c > w->len)                                                       \
+            return false;                                                     \
+        if (w->first)                                                         \
+            return w->cur >= c;                                               \
+        else                                                                  \
+            return true;                                                      \
+    }
+
+#define CONV_LOW_PASS_UPDATE_API__(NAME, TYPE)                                \
+    void NAME##_update(NAME *, TYPE);
+
+#define CONV_LOW_PASS_UPDATE__(NAME, TYPE)                                    \
+                                                                              \
+    void NAME##_update(NAME *w, TYPE value) {                                 \
+        TYPE sum = (TYPE)0;                                                   \
+                                                                              \
+        const unsigned int div = (w->first) ? (w->cur + 1) : (w->len);        \
+                                                                              \
+        w->win[w->cur] = value;                                               \
+        w->cur++;                                                             \
+        if (w->cur >= w->len)                                                 \
+                w->first = false;                                             \
+        w->cur %= w->len;                                                     \
+                                                                              \
+        for (unsigned int i = 0; i < div; ++i)                                \
+            sum += w->win[i];                                                 \
+                                                                              \
+        w->avg = (TYPE)(sum / (TYPE)div);                                     \
+    }
+
+
+#define CONV_LOW_PASS_MEDIAN_API(NAME, TYPE) \
+    TYPE NAME##_median(NAME *, TYPE *);
+
+/**
+ * Implement moving median resolution based on convolution buffer.
+ *
+ * Provide a source for standard qsort for this (#include <stdlib.h>).
+ * TYPE must be directly comparable with <, >, ==.
+ */
+#define CONV_LOW_PASS_MEDIAN(NAME, TYPE)                                      \
+    static int NAME##_compare(void const *left, void const *right) {          \
+        const TYPE lv = *(TYPE const *)left;                                  \
+        const TYPE rv = *(TYPE const *)right;                                 \
+        if (lv < rv)                                                          \
+            return -1;                                                        \
+        else if (lv == rv)                                                    \
+            return 0;                                                         \
+        else                                                                  \
+            return 1;                                                         \
+    }                                                                         \
+                                                                              \
+    TYPE NAME##_median(NAME *w, TYPE *tmp) {                                  \
+        const unsigned int in_use = (w->first) ? (w->cur) : (w->len);         \
+        if (in_use == 0)                                                      \
+            return 0;                                                         \
+                                                                              \
+        memcpy(tmp, w->win, in_use * sizeof(TYPE));                           \
+        qsort(tmp, in_use, sizeof(TYPE), NAME##_compare);                     \
+                                                                              \
+        if ((in_use % 2) == 1)                                                \
+            return tmp[in_use / 2];                                           \
+        else                                                                  \
+            return (tmp[in_use / 2] + tmp[(in_use / 2) - 1]) / 2;             \
+    }
+
+/**
+ * @}
+ * @}
+ */
+
+#endif /* INC_CONVOLUTION_LOW_PASS_H */
---
+++ test/t-convolution-low-pass.c
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2021 micro framework workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <common/compiler.h>
+#include <test/tap.h>
+
+#include <c/convolution-low-pass.h>
+
+CONV_LOW_PASS_API(clp, int)
+CONV_LOW_PASS(clp, int)
+
+CONV_LOW_PASS_MEDIAN_API(clp, int)
+CONV_LOW_PASS_MEDIAN(clp, int)
+
+#define T_SIZE 8
+
+int
+main(UNUSED int argc, UNUSED char *argv[])
+{
+    const int data[] = {11, 22, 33, 44, 55, 66, 77, 88};
+    const int null_field[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int temp[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int buffer[T_SIZE];
+
+    plan(39);
+
+    clp w;
+    clp_init(&w, buffer, T_SIZE);
+    ok(memcmp(w.win, null_field, sizeof(null_field)) == 0, "init correct");
+    ok(w.first == true, "not full");
+    ok(clp_avg(&w) == 0, "empty average gives 0");
+    ok(clp_median(&w, temp) == 0, "empty average gives 0");
+
+    clp_update(&w, data[0]);
+
+    ok(clp_has_min_values(&w, 1) == true, "one value");
+    ok(clp_has_min_values(&w, 8) == false, "has not 8 values");
+    ok(clp_has_min_values(&w, 100) == false, "has not 100 values");
+    ok(w.first == true, "not full");
+
+    ok(clp_avg(&w) == data[0], "single value is average");
+    ok(clp_median(&w, temp) == data[0], "single value is median");
+
+    for (int i = 1; i < T_SIZE - 1; ++i)
+        clp_update(&w, data[i]);
+
+    ok(clp_has_min_values(&w, 1) == true, "one value");
+    ok(clp_has_min_values(&w, 7) == true, "has 7 values");
+    ok(clp_has_min_values(&w, 8) == false, "has not 8 values");
+    ok(clp_has_min_values(&w, 100) == false, "has not 100 values");
+    ok(w.first == true, "not full");
+
+    ok(clp_avg(&w) == 44, "average of seven is 44");
+    ok(clp_median(&w, temp) == 44, "median is also 44");
+
+    clp_update(&w, data[7]);
+    ok(w.first == false, "not full");
+
+    ok(clp_avg(&w) == 49, "average is 49.5, integer truncate is 49");
+    ok(memcmp(w.win, data, sizeof(data)) == 0, "values stored correctly");
+
+    ok(clp_median(&w, temp) == 49, "median is also 49.5 (49)");
+    ok(memcmp(temp, data, sizeof(data)) == 0, "sorted values still sorted");
+
+    clp_update(&w, data[T_SIZE - 1]);
+    ok(w.first == false, "enter second iteration");
+
+    for (int i = T_SIZE - 2; i >= 0; --i)
+        clp_update(&w, data[i]);
+
+    ok(clp_has_min_values(&w, 1) == true, "one value");
+    ok(clp_has_min_values(&w, 8) == true, "has 8 values");
+    ok(clp_has_min_values(&w, 100) == false, "has not 100 values");
+    ok(w.first == false, "still beyond first iteration");
+
+    ok(clp_avg(&w) == 49, "average is 49.5, integer truncate is 49");
+    ok(memcmp(w.win, data, sizeof(data)) != 0, "values stored in other order");
+
+    ok(clp_median(&w, temp) == 49, "median is also 49.5 (49)");
+    ok(memcmp(temp, data, sizeof(data)) == 0, "values get sorted");
+
+    for (int i = T_SIZE - 1; i >= 0; --i)
+        clp_update(&w, data[i]);
+
+    ok(clp_has_min_values(&w, 1) == true, "one value");
+    ok(clp_has_min_values(&w, 8) == true, "has 8 values");
+    ok(clp_has_min_values(&w, 100) == false, "has not 100 values");
+    ok(w.first == false, "still beyond first iteration");
+
+    ok(clp_avg(&w) == 49, "average is 49.5, integer truncate is 49");
+    ok(memcmp(w.win, data, sizeof(data)) != 0, "values stored in other order");
+
+    ok(clp_median(&w, temp) == 49, "median is also 49.5 (49)");
+    ok(memcmp(temp, data, sizeof(data)) == 0, "values get sorted");
+
+    return EXIT_SUCCESS;
+}
