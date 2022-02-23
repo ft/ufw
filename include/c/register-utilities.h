e291063970d5cb651c8a095149bb0c104ae108b3
Frank Terbeck <ft@bewatermyfriend.org>
2022-02-23 15:49:27 +0100

    reg,utils: Make printer target opaque and allow exchanging printer
    Printing to FILE* is fine for native builds, but in other environments, its not
    perfect. For instance, when these pretty printers are to be used within ze-
    phyr's shell environment, we'd want to pass struct shell* through. This change
    allows this. Since any pointer automatically converts to void*, this should not
    break existing code.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- include/c/register-utilities.h
+++ include/c/register-utilities.h
@@ -9,14 +9,17 @@ extern "C" {
 
 #include <c/register-table.h>
 
-void register_table_print(FILE*, const char*, const RegisterTable*);
-void register_area_print(FILE*, const char*, const RegisterArea*);
-void register_entry_print(FILE*, const char*, const RegisterEntry*);
-void register_init_print(FILE*, const char*, RegisterInit);
+void register_table_print(void*, const char*, const RegisterTable*);
+void register_area_print(void*, const char*, const RegisterArea*);
+void register_entry_print(void*, const char*, const RegisterEntry*);
+void register_init_print(void*, const char*, RegisterInit);
 char *register_accesscode_to_string(RegisterAccessCode);
 char *register_initcode_to_string(RegisterInitCode);
 char *register_registertype_to_string(RegisterType);
 char *register_validatortype_to_string(RegisterValidatorType);
+
+typedef void (*fprintf_like)(void*, const char*, ...);
+void register_set_printer(fprintf_like);
 
 #ifdef __cplusplus
 }
--- src/c/register-utilities.c
+++ src/c/register-utilities.c
@@ -5,6 +5,7 @@
  */
 
 #include <inttypes.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -14,120 +15,137 @@
 
 #define IDX2STR(x) [x] = #x
 
+static void
+rimpl_fprintf(void *dst, const char *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(dst, fmt, args);
+	va_end(args);
+}
+
+static fprintf_like r_fprintf = rimpl_fprintf;
+
 void
-register_table_print(FILE *fh, const char *prefix, const RegisterTable *t)
+register_set_printer(fprintf_like p)
+{
+	r_fprintf = p;
+}
+
+void
+register_table_print(void *fh, const char *prefix, const RegisterTable *t)
 {
     const bool is_initialised = BIT_ISSET(t->flags, REG_TF_INITIALISED);
-    fprintf(fh, "%sRegisterTable ", prefix);
+    r_fprintf(fh, "%sRegisterTable ", prefix);
     if (is_initialised == false) {
-        fprintf(fh, "is NOT initialised!\n");
+        r_fprintf(fh, "is NOT initialised!\n");
     } else {
-        fprintf(fh, "is initialised.\n");
-        fprintf(fh, "%s     areas: %" PRIu16 "\n", prefix, t->areas);
-        fprintf(fh, "%s   entries: %" PRIu32 "\n", prefix, t->entries);
+        r_fprintf(fh, "is initialised.\n");
+        r_fprintf(fh, "%s     areas: %" PRIu16 "\n", prefix, t->areas);
+        r_fprintf(fh, "%s   entries: %" PRIu32 "\n", prefix, t->entries);
     }
 
-    fprintf(fh, "%s\n%sList of Areas:\n", prefix, prefix);
+    r_fprintf(fh, "%s\n%sList of Areas:\n", prefix, prefix);
     AreaHandle area = 0;
     while ((area < AREA_HANDLE_MAX) && !(is_end_of_areas(t->area + area))) {
         RegisterArea *a = t->area + area;
-        fprintf(fh, "%s  AreaID: %" PRIu16 "\n", prefix, area);
+        r_fprintf(fh, "%s  AreaID: %" PRIu16 "\n", prefix, area);
         register_area_print(fh, prefix, a);
         area += 1;
     }
 
-    fprintf(fh, "%s\n%sList of Entries:\n", prefix, prefix);
+    r_fprintf(fh, "%s\n%sList of Entries:\n", prefix, prefix);
     RegisterHandle reg = 0;
     while ((reg < AREA_HANDLE_MAX) && !(is_end_of_entries(t->entry + reg))) {
         RegisterEntry *e = t->entry + reg;
-        fprintf(fh, "%s  RegisterEntryID: %" PRIu32 "\n", prefix, reg);
+        r_fprintf(fh, "%s  RegisterEntryID: %" PRIu32 "\n", prefix, reg);
         register_entry_print(fh, prefix, e);
         reg += 1;
     }
 }
 
 void
-register_area_print(FILE *fh, const char *prefix, const RegisterArea *a)
+register_area_print(void *fh, const char *prefix, const RegisterArea *a)
 {
-    fprintf(fh, "%s    Area Start: 0x%08" PRIx32 "\n", prefix, a->base);
-    fprintf(fh, "%s    Area Size : 0x%08" PRIx32 "\n", prefix, a->size);
-    fprintf(fh, "%s    Area Flags: 0x%08" PRIx32 "\n", prefix,
-            (uint32_t)a->flags);
-    fprintf(fh, "%s    Area has %s read method.\n", prefix,
-            a->read == NULL ? "no" : "a");
-    fprintf(fh, "%s    Area has %s write method.\n", prefix,
-            a->write == NULL ? "no" : "a");
+    r_fprintf(fh, "%s    Area Start: 0x%08" PRIx32 "\n", prefix, a->base);
+    r_fprintf(fh, "%s    Area Size : 0x%08" PRIx32 "\n", prefix, a->size);
+    r_fprintf(fh, "%s    Area Flags: 0x%08" PRIx32 "\n", prefix,
+              (uint32_t)a->flags);
+    r_fprintf(fh, "%s    Area has %s read method.\n", prefix,
+              a->read == NULL ? "no" : "a");
+    r_fprintf(fh, "%s    Area has %s write method.\n", prefix,
+              a->write == NULL ? "no" : "a");
 }
 
 void
-register_entry_print(FILE *fh, const char *prefix, const RegisterEntry *e)
+register_entry_print(void *fh, const char *prefix, const RegisterEntry *e)
 {
-    fprintf(fh, "%s    Register Name   : %s\n", prefix,
-            e->name != NULL ? e->name : "<UNNAMED-REGISTER>");
-    fprintf(fh, "%s    Register Type   : %s\n", prefix,
-            register_registertype_to_string(e->type));
-    fprintf(fh, "%s    Register Flags  : 0x%08" PRIx32 "\n", prefix,
-            (uint32_t)e->flags);
-    fprintf(fh, "%s    Register Address: 0x%08" PRIx32 "\n", prefix, e->address);
+    r_fprintf(fh, "%s    Register Name   : %s\n", prefix,
+              e->name != NULL ? e->name : "<UNNAMED-REGISTER>");
+    r_fprintf(fh, "%s    Register Type   : %s\n", prefix,
+              register_registertype_to_string(e->type));
+    r_fprintf(fh, "%s    Register Flags  : 0x%08" PRIx32 "\n", prefix,
+              (uint32_t)e->flags);
+    r_fprintf(fh, "%s    Register Address: 0x%08" PRIx32 "\n", prefix, e->address);
 }
 
 void
-register_init_print(FILE *fh, const char *prefix, const RegisterInit result)
+register_init_print(void *fh, const char *prefix, const RegisterInit result)
 {
-    fprintf(fh, "%sRegister Init Code: %s\n", prefix,
-            register_initcode_to_string(result.code));
+    r_fprintf(fh, "%sRegister Init Code: %s\n", prefix,
+              register_initcode_to_string(result.code));
     switch (result.code) {
     case REG_INIT_TABLE_INVALID:
-        fprintf(fh, "%sBasic Initialisation Error!\n", prefix);
+        r_fprintf(fh, "%sBasic Initialisation Error!\n", prefix);
         break;
     case REG_INIT_NO_AREAS:
-        fprintf(fh, "%sSupplied area table is empty!\n", prefix);
+        r_fprintf(fh, "%sSupplied area table is empty!\n", prefix);
         break;
     case REG_INIT_TOO_MANY_AREAS:
-        fprintf(fh, "%sSupplied area table has more"
-                " than AREA_HANDLE_MAX entries!\n", prefix);
+        r_fprintf(fh, "%sSupplied area table has more"
+                  " than AREA_HANDLE_MAX entries!\n", prefix);
         break;
     case REG_INIT_AREA_INVALID_ORDER:
-        fprintf(fh, "%sAddresses in area table is not linear!\n", prefix);
-        fprintf(fh, "%sFirst offending area: %" PRIu16 "!\n", prefix,
-                result.pos.area);
+        r_fprintf(fh, "%sAddresses in area table is not linear!\n", prefix);
+        r_fprintf(fh, "%sFirst offending area: %" PRIu16 "!\n", prefix,
+                  result.pos.area);
         break;
     case REG_INIT_AREA_ADDRESS_OVERLAP:
-        fprintf(fh, "%sAddresses of areas overlap!\n", prefix);
-        fprintf(fh, "%sFirst offending area: %" PRIu16 "!\n", prefix,
-                result.pos.area);
+        r_fprintf(fh, "%sAddresses of areas overlap!\n", prefix);
+        r_fprintf(fh, "%sFirst offending area: %" PRIu16 "!\n", prefix,
+                  result.pos.area);
         break;
     case REG_INIT_TOO_MANY_ENTRIES:
-        fprintf(fh, "%sSupplied entry table has more"
-                " than REGISTER_HANDLE_MAX entries!\n", prefix);
+        r_fprintf(fh, "%sSupplied entry table has more"
+                  " than REGISTER_HANDLE_MAX entries!\n", prefix);
         break;
     case REG_INIT_ENTRY_INVALID_ORDER:
-        fprintf(fh, "%sAddresses in entry table is not linear!\n", prefix);
-        fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
-                result.pos.entry);
+        r_fprintf(fh, "%sAddresses in entry table is not linear!\n", prefix);
+        r_fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
+                  result.pos.entry);
         break;
     case REG_INIT_ENTRY_ADDRESS_OVERLAP:
-        fprintf(fh, "%sAddresses of entries overlap!\n", prefix);
-        fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
-                result.pos.entry);
+        r_fprintf(fh, "%sAddresses of entries overlap!\n", prefix);
+        r_fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
+                  result.pos.entry);
         break;
     case REG_INIT_ENTRY_IN_MEMORY_HOLE:
-        fprintf(fh, "%sMemory of entry in unmapped address-space!\n", prefix);
-        fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
-                result.pos.entry);
+        r_fprintf(fh, "%sMemory of entry in unmapped address-space!\n", prefix);
+        r_fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
+                  result.pos.entry);
         break;
     case REG_INIT_ENTRY_INVALID_DEFAULT:
-        fprintf(fh, "%sEntry defines default value that does not validate!\n",
-                prefix);
-        fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
-                result.pos.entry);
+        r_fprintf(fh, "%sEntry defines default value that does not validate!\n",
+                  prefix);
+        r_fprintf(fh, "%sFirst offending entry: %" PRIu32 "!\n", prefix,
+                  result.pos.entry);
         break;
     case REG_INIT_SUCCESS:
-        fprintf(fh, "%sRegister Table Initialisation Successful!\n", prefix);
+        r_fprintf(fh, "%sRegister Table Initialisation Successful!\n", prefix);
         break;
     default:
-        fprintf(fh, "%sUnexpected result code: 0x%04x\n", prefix, result.code);
-        fprintf(fh, "%sHow did this happen?\n", prefix);
+        r_fprintf(fh, "%sUnexpected result code: 0x%04x\n", prefix, result.code);
+        r_fprintf(fh, "%sHow did this happen?\n", prefix);
         break;
     }
 }
