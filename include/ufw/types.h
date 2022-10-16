4342c6ab1791503f4b0e798e92961ca85ba95a2a
Frank Terbeck <ft@bewatermyfriend.org>
2022-10-16 14:20:57 +0200

    Add generic source and sink data types
    These are types that allow adapting many data endpoints to generic algorithms,
    like SLIP and similar.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ include/ufw/types.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2022 ufw workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+#ifndef INC_UFW_TYPES_H
+#define INC_UFW_TYPES_H
+
+#include <stddef.h>
+
+#include <ufw/compat/ssize-t.h>
+
+/**
+ * Function type that accepts an octet
+ *
+ * These functions return integer values and accept an arbitrary data pointer,
+ * which the individual application may optionally use. The byte parameter is
+ * the data to be transfered into the sink.
+ */
+typedef int (*OctetSink)(void*, unsigned char);
+
+/**
+ * Function type that produces an octet
+ *
+ * These functions return integer values (negative values indicating an error)
+ * and accept an arbitrary data pointer, which the individual application may
+ * optionally use. The second byte parameter pointer is used to return the
+ * produced octet.
+ */
+typedef int (*OctetSource)(void*, void*);
+
+/** Function type that accepts a buffer of octets */
+typedef ssize_t (*ChunkSink)(void*, const void*, size_t);
+
+/** Function type that produces a buffer of octets */
+typedef ssize_t (*ChunkSource)(void*, void*, size_t);
+
+typedef enum ufw_data_kind {
+    DATA_KIND_OCTET,
+    DATA_KIND_CHUNK
+} DataKind;
+
+typedef struct ufw_source {
+    DataKind kind;
+    void *driver;
+    union {
+        OctetSource octet;
+        ChunkSource chunk;
+    } source;
+} Source;
+
+typedef struct ufw_sink {
+    DataKind kind;
+    void *driver;
+    union {
+        OctetSink octet;
+        ChunkSink chunk;
+    } sink;
+} Sink;
+
+#define OCTET_SOURCE_INIT(CB, DRIVER) { \
+        .kind = DATA_KIND_OCTET,        \
+        .driver = DRIVER,               \
+        .source.octet = CB }
+
+#define CHUNK_SOURCE_INIT(CB, DRIVER) { \
+        .kind = DATA_KIND_CHUNK,        \
+        .driver = DRIVER,               \
+        .source.chunk = CB }
+
+#define OCTET_SINK_INIT(CB, DRIVER) {   \
+        .kind = DATA_KIND_OCTET,        \
+        .driver = DRIVER,               \
+        .sink.octet = CB }
+
+#define CHUNK_SINK_INIT(CB, DRIVER) {   \
+        .kind = DATA_KIND_CHUNK,        \
+        .driver = DRIVER,               \
+        .sink.chunk = CB }
+
+void octet_source_init(Source*, OctetSource, void*);
+void chunk_source_init(Source*, ChunkSource, void*);
+void octet_sink_init(Sink*, OctetSink, void*);
+void chunk_sink_init(Sink*, ChunkSink, void*);
+
+int source_get_octet(Source*, void*);
+int sink_put_octet(Sink*, unsigned char);
+
+ssize_t source_get_chunk(Source*, void*, size_t);
+ssize_t sink_put_chunk(Sink*, const void*, size_t);
+
+#endif /* INC_UFW_TYPES_H */
---
+++ src/source-sink.c
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2022 ufw workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+/**
+ * @file source-sink.c
+ * @brief Implementation of a Sink and Source abstraction
+ *
+ * This implements a generic sink and source data type. The main idea here is
+ * to be able to reuse implementations of protocols like SLIP for a wide array
+ * of applications.
+ *
+ * The API implements getting and putting octets and buffers of octets from/to
+ * sources/sinks. The underlying driver can be either of those access paradigms
+ * and the abstraction implements the other on top of it.
+ *
+ * Sources and sinks make sure the expected amount of data is transmitted,
+ * unlike the POSIX read() and write() functions, for instance.
+ *
+ * The OctetSource/OctetSink and ChunkSource/ChunkSource function types need to
+ * therefore behave similar to those functions, while returning negative errno
+ * instead of actually setting errno. They do need to return the number of
+ * octets transmitted. Returning zero indicates end-of-file. If some condition
+ * would cause the function to return zero even though EOF was not reached yet,
+ * an appropriate errno has to be used. -EINTR in case the function was
+ * interrupted by a signal before an octet could be read/written.
+ *
+ * If a driver returns zero (indicating end-of-file) while getting/putting a
+ * buffer of octets, the API returns zero as well, regardless of any previously
+ * transmitted data.
+ *
+ * The API for sources and sinks return negative errno values to signal errors.
+ *
+ * For sources, returning zero indicates end of file.
+ *
+ * Positive return values indicate the number of octets that were transmitted.
+ *
+ * Using a data count of zero, or one bigger than SSIZE_MAX causes the API to
+ * return -EINVAL.
+ */
+
+#include <stddef.h>
+
+#include <ufw/compat/errno.h>
+#include <ufw/compat/ssize-t.h>
+
+#include <ufw/types.h>
+
+void
+octet_source_init(Source *instance, OctetSource source, void *driver)
+{
+    instance->kind = DATA_KIND_OCTET;
+    instance->source.octet = source;
+    instance->driver = driver;
+}
+
+void
+chunk_source_init(Source *instance, ChunkSource source, void *driver)
+{
+    instance->kind = DATA_KIND_CHUNK;
+    instance->source.chunk = source;
+    instance->driver = driver;
+}
+
+void
+octet_sink_init(Sink *instance, OctetSink sink, void *driver)
+{
+    instance->kind = DATA_KIND_OCTET;
+    instance->sink.octet = sink;
+    instance->driver = driver;
+}
+
+void
+chunk_sink_init(Sink *instance, ChunkSink sink, void *driver)
+{
+    instance->kind = DATA_KIND_CHUNK;
+    instance->sink.chunk = sink;
+    instance->driver = driver;
+}
+
+int
+source_get_octet(Source *source, void *data)
+{
+    return source->kind == DATA_KIND_OCTET
+        ? source->source.octet(source->driver, data)
+        : source->source.chunk(source->driver, data, 1u);
+}
+
+int
+sink_put_octet(Sink *sink, const unsigned char data)
+{
+    return sink->kind == DATA_KIND_OCTET
+        ? sink->sink.octet(sink->driver, data)
+        : sink->sink.chunk(sink->driver, &data, 1u);
+}
+
+static inline ssize_t
+source_adapt(OctetSource source, void *driver, void *buf, size_t n)
+{
+    unsigned char *data = buf;
+    for (size_t i = 0u; i < n; ++i) {
+        const int rc = source(driver, data + i);
+        if (rc < 0) {
+            return (ssize_t)rc;
+        }
+    }
+
+    return 0;
+}
+
+static inline ssize_t
+once_source_get_chunk(Source *source, void *buf, size_t n)
+{
+    if (n == 0 || n > SSIZE_MAX) {
+        return -EINVAL;
+    }
+
+    return source->kind == DATA_KIND_OCTET
+        ? source_adapt(source->source.octet, source->driver, buf, n)
+        : source->source.chunk(source->driver, buf, n);
+}
+
+ssize_t
+source_get_chunk(Source *source, void *buf, size_t n)
+{
+    if (n == 0 || n > SSIZE_MAX) {
+        return -EINVAL;
+    }
+
+    size_t rest = n;
+    while (rest > 0) {
+        const ssize_t get = once_source_get_chunk(source, buf, rest);
+        if (get < 0) {
+            return get;
+        } else if (get == 0) {
+            return 0;
+        }
+        rest -= get;
+    }
+
+    return (ssize_t)n;
+}
+
+static inline ssize_t
+sink_adapt(OctetSink sink, void *driver, const void *buf, size_t n)
+{
+    const unsigned char *data = buf;
+    for (size_t i = 0u; i < n; ++i) {
+        const int rc = sink(driver, data[i]);
+        if (rc < 0) {
+            return (ssize_t)rc;
+        }
+    }
+
+    return n;
+}
+
+static inline ssize_t
+once_sink_put_chunk(Sink *sink, const void *buf, size_t n)
+{
+    return sink->kind == DATA_KIND_OCTET
+        ? sink_adapt(sink->sink.octet, sink->driver, buf, n)
+        : sink->sink.chunk(sink->driver, buf, n);
+}
+
+ssize_t
+sink_put_chunk(Sink *sink, const void *buf, size_t n)
+{
+    if (n == 0 || n > SSIZE_MAX) {
+        return -EINVAL;
+    }
+
+    size_t rest = n;
+    while (rest > 0) {
+        const ssize_t put = once_sink_put_chunk(sink, buf, rest);
+        if (put < 0) {
+            return put;
+        } else if (put == 0) {
+            return 0;
+        }
+        rest -= put;
+    }
+
+    return (ssize_t)n;
+}
