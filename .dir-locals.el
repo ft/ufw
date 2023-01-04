eb97dc18e8bfbfe7852dfeafb9f88c959f9cc5b9
Frank Terbeck <ft@bewatermyfriend.org>
2023-01-04 22:04:14 +0100

    Add dir-locals for some emacs convenience
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil
+  (meta:project-author  . "micro framework workers")
+  (meta:project-licence . "LICENCE")
+  (meta:project-code "include"
+                     "src"
+                     "scheme")))
