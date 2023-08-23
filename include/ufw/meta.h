3217dcc40048d39e9d4fb049f303280f24911d74
Frank Terbeck <ft@bewatermyfriend.org>
2023-08-24 00:54:45 +0200

    Add ufw/meta.h to expose some library meta information
    For now, this only exposes the library version as preprocessor macros.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ include/ufw/meta.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 micro framework workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+/**
+ * @file meta.h
+ * @brief Micro Framework Meta Information.
+ */
+
+#ifndef INC_UFW_META_H_79507d68
+#define INC_UFW_META_H_79507d68
+
+/** Micro Framework Library Major Version Number */
+#define UFW_LIBRARY_MAJOR 4u
+
+/** Micro Framework Library Minor Version Number */
+#define UFW_LIBRARY_MINOR 0u
+
+/** Micro Framework Library Patchlevel */
+#define UFW_LIBRARY_PATCH 0u
+
+#endif /* INC_UFW_META_H_79507d68 */
