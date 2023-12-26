bf698e732ea7da6f6860cc55b67f810f86d337b3
Frank Terbeck <ft@bewatermyfriend.org>
2023-12-26 20:21:46 +0100

    Add a generic block allocator type
    The intention here is to be able to interface with various block memory
    allocators such as many slab allocators found in many RTOSes.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ include/ufw/allocator.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023 micro framework workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+#ifndef INC_UFW_ALLOCATOR_H_3ff7ef48
+#define INC_UFW_ALLOCATOR_H_3ff7ef48
+
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef enum Allocator {
+    UFW_ALLOC_GENERIC,
+    UFW_ALLOC_SLAB
+} Allocator;
+
+typedef int (*GenericAlloc)(void*, void**, size_t);
+typedef int (*SlabAlloc)(void*, void**);
+typedef void (*GenericFree)(void*, void*);
+
+typedef struct BlockAllocator {
+    Allocator type;
+    size_t blocksize;
+    void *driver;
+    union {
+        GenericAlloc generic;
+        SlabAlloc slab;
+    } alloc;
+    GenericFree free;
+} BlockAllocator;
+
+int ufw_malloc(void*, void**, size_t);
+void ufw_mfree(void*, void*);
+
+int block_alloc(BlockAllocator*, void**);
+void block_free(BlockAllocator*, void*);
+
+#define MAKE_GENERIC_BLOCKALLOC(_driver, _alloc, _free, _blocksize) \
+    { .type = UFW_ALLOC_GENERIC,                                    \
+      .blocksize = _blocksize,                                      \
+      .driver = _driver,                                            \
+      .alloc.generic = _alloc,                                      \
+      .free = _free }
+
+#define MAKE_SLAB_BLOCKALLOC(_driver, _alloc, _free, _blocksize) \
+    { .type = UFW_ALLOC_SLAB,                                    \
+      .blocksize = _blocksize,                                   \
+      .driver = _driver,                                         \
+      .alloc.slab = _alloc,                                      \
+      .free = _free }
+
+#define MAKE_STDHEAD_BLOCKALLOC(_blocksize) \
+    { .type = UFW_ALLOC_GENERIC,            \
+      .blocksize = _blocksize,              \
+      .driver = NULL,                       \
+      .alloc.generic = ufw_malloc,          \
+      .free = ufw_mfree }
+
+#endif /* INC_UFW_ALLOCATOR_H_3ff7ef48 */
---
+++ src/allocator.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 micro framework workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <ufw/compat/errno.h>
+
+#include <ufw/allocator.h>
+#include <ufw/compiler.h>
+
+int
+ufw_malloc(UNUSED void *driver, void **m, size_t n)
+{
+    *m = malloc(n);
+    return (*m == NULL) ? -ENOMEM : 0;
+}
+
+void
+ufw_mfree(UNUSED void *driver, void *m)
+{
+    free(m);
+}
+
+int
+block_alloc(BlockAllocator *ba, void **m)
+{
+    if (ba->type == UFW_ALLOC_GENERIC) {
+        return ba->alloc.generic(ba->driver, m, ba->blocksize);
+    } else {
+        return ba->alloc.slab(ba->driver, m);
+    }
+}
+
+void
+block_free(BlockAllocator *ba, void *m)
+{
+    ba->free(ba->driver, m);
+}
