d1a57996ff42e063892527cb7362f2869df3a61e
Frank Terbeck <ft@bewatermyfriend.org>
2023-12-30 01:58:21 +0100

    ep: Add a continuable sink implementation
    See the included documentation for a rationale.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ include/ufw/endpoints/continuable-sink.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2023 micro framework workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+/**
+ * @file continuable-sink.h
+ * @brief API for continuable sinks
+ *
+ * A continuable sink is a type of Sink, interfacing a memory buffer, that
+ * allows for the sink to still accept data, even if there is some sort of
+ * problem with the associated buffer.
+ *
+ * This is useful when implementing communication protocols, when it is
+ * desirable to be able to gracefully react to memory related issues, when
+ * the remote side cannot immediately see that reaction.
+ *
+ * The main idea here is to use a BlockAllocator to fetch the main buffer
+ * for the sink to operate on. If this allocation fails, or if this buffer
+ * would be overflown, the sink makes a note of this and keeps accepting,
+ * but discarding additional data.
+ *
+ * As an extension, it if possible to provide the sink with a fallback buf-
+ * fer, that the sink can store some data in, even if the block allocation
+ * failed.
+ *
+ * Allocation only happens once and that is the first time data arrives for
+ * the sink to store. When allocation succeeds, a user specifiable callback
+ * is called that allows further setup of the OctetBuffer before the sink
+ * takes up its normal operation.
+ *
+ * If no allocator is provided, the sink will only operate on the fallback
+ * buffer. If that is also not provided, the sink basically behaves like
+ * the trivial "null" sink (see ufw/endpoints.h for details).
+ */
+
+#ifndef INC_UFW_ENDPOINTS_CONTINUABLE_SINK_H_11229582
+#define INC_UFW_ENDPOINTS_CONTINUABLE_SINK_H_11229582
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <ufw/allocator.h>
+#include <ufw/endpoints.h>
+#include <ufw/octet-buffer.h>
+
+typedef struct ContinuableIssue {
+    /*
+     * Error number value to encode possible conditions:
+     *
+     * - EBUSY: Memory allocation failed. This causes the sink runner to store
+     *   the beginning of the frame into the fallback buffer. Any data that
+     *   does not fit into this buffer will be discarded until the end of the
+     *   current frame is reached. When this errno value is retrieved by
+     *   regp_recv(), it will send a EBUSY reponse to the remote side.
+     *
+     * - ENOMEM: The frame is too large to fit into the allocated buffer. When
+     *   this happens, any data that cannot be store in the buffer be discarded
+     *   until the end of the current frame is reached. With this errno value,
+     *   regp_recv() will send a ERXOVERFLOW response to the remote side.
+     *
+     * If everything went well, this value should be zero.
+     */
+    int id;
+    size_t datacount;
+} ContinuableIssue;
+
+typedef struct ContinuableSink {
+    /* This is the allocator that will be used to allocate a fresh receive
+     * buffer, for this sink to store data into. This may be NULL, in which
+     * case it is obviously not used. In that case, some other memory has to
+     * be linked to the frame octet buffer below. */
+    BlockAllocator *alloc;
+    /* An octet buffer to navigate the dynamically allocated buffer. */
+    OctetBuffer buffer;
+    /* This OctetBuffer will be linked to the static header store below. */
+    OctetBuffer *fallback;
+    /* A callback to run after allocation. This can be used to do any desired
+     * setup of the OctetBuffer instance. This is called after the allocated
+     * buffer is linked into the OctetBuffer. In our case, this will move the
+     * buffer offset so we can store a RPFrame instance at the beginning of the
+     * block. */
+    void (*postalloc)(OctetBuffer*);
+    ContinuableIssue error;
+} ContinuableSink;
+
+#define CONTINUABLE_SINK(ALLOC,FB,CB) {     \
+        .alloc = (ALLOC),                   \
+        .buffer = OCTET_BUFFER(NULL, 0u),   \
+        .fallback = (FB),                   \
+        .postalloc = (CB),                  \
+        .error.id = 0,                      \
+        .error.datacount = 0u               }
+
+void continuable_sink_init(Sink*, ContinuableSink*);
+
+#endif /* INC_UFW_ENDPOINTS_CONTINUABLE_SINK_H_11229582 */
---
+++ src/endpoints/continuable-sink.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2023 micro framework workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+#include <ufw/allocator.h>
+#include <ufw/compat/errno.h>
+#include <ufw/endpoints.h>
+#include <ufw/endpoints/continuable-sink.h>
+
+static int
+cs_add(ContinuableSink *cs, const void *data, const size_t n)
+{
+    OctetBuffer *b = cs->buffer.data != NULL ? &cs->buffer : cs->fallback;
+    if (b == NULL) {
+        return -ENOMEM;
+    }
+    const size_t rest = octet_buffer_rest(b);
+    const size_t tosave = n < rest ? n : rest;
+    octet_buffer_add(b, data, tosave);
+    return tosave < n ? -ENOMEM : 0;
+}
+
+static ssize_t
+run_continuable_sink(void *driver, const void *data, size_t n)
+{
+    ContinuableSink *cs = driver;
+
+    if (cs->error.id != 0) {
+        /* If there's still room in a buffer, save as much as possible, but
+         * we're already in a bad state. */
+        (void)cs_add(cs, data, n);
+        cs->error.datacount += n;
+        return n;
+    }
+
+    if (cs->alloc == NULL && cs->fallback == NULL) {
+        /* No buffers? Can't do much with that. */
+        cs->error.id = ENOMEM;
+        cs->error.datacount += n;
+        return n;
+    }
+
+    if (cs->buffer.data == NULL && cs->alloc != NULL) {
+        /* We do have an allocator, but no allocated buffer. If allocation
+         * would have failed previously, we wouldn't be here, because error.id
+         * would be set to something non-zero. That means, this is the first
+         * (and only) time we're trying to allocate the buffer. */
+        void *buf;
+        int rc = block_alloc(cs->alloc, &buf);
+        if (rc < 0) {
+            /* Allocation failed. Make a note of that... */
+            cs->error.id = EBUSY;
+            cs->error.datacount = n;
+            cs->buffer.data = NULL;
+            /* ...and try to save as much data as possible in the fallback
+             * buffer, if we have one. */
+            (void)cs_add(cs, data, n);
+            return n;
+        }
+        /* Allocation succeeded. Call postalloc if we have it. */
+        cs->buffer.data = buf;
+        cs->buffer.size = cs->alloc->blocksize;
+        if (cs->postalloc != NULL) {
+            cs->postalloc(&cs->buffer);
+        }
+    }
+
+    /* In normal operation, we're here. */
+    const size_t m = cs->buffer.data != NULL
+        ? cs->buffer.used
+        : (cs->fallback != NULL
+           ? cs->fallback->used
+           : 0u);
+    const int rc = cs_add(cs, data, n);
+
+    if (rc < 0) {
+        cs->error.id = ENOMEM;
+        cs->error.datacount = m+n;
+    }
+
+    return n;
+}
+
+void
+continuable_sink_init(Sink *instance, ContinuableSink *driver)
+{
+    driver->buffer.data = NULL;
+    driver->buffer.size = 0u;
+    driver->buffer.offset = 0u;
+    driver->buffer.used = 0u;
+    driver->error.id = 0u;
+    driver->error.datacount = 0u;
+    chunk_sink_init(instance, run_continuable_sink, driver);
+}
