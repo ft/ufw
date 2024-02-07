128e4b416ba5b954d7398a39294709e52b815e36
Frank Terbeck <ft@bewatermyfriend.org>
2024-02-08 00:09:16 +0100

    Add toolchain file for arm-zephyr-eabi
    This allows using zephyr's ARM SDK compiler for arbitrary code. The toolchain
    is basically a blessed GNU toolchain with a number of included batteries. It
    works much like gnu-arm-none-eabi, and thus this automatically supports running
    resulting binaries in QEMU.
    
    To add this to mmh, you can do this in ~/.makemehappy/config.yaml (or whereever
    in its configuration stack you prefer):
    
        toolchains:
          - name: arm-zephyr-eabi
            architecture: [ cortex-m3 ]
    
    …adjust to taste.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ cmake/toolchains/arm-zephyr-eabi.cmake
@@ -0,0 +1,31 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(TOOLCHAIN_ID "gcc-arm")
+set(COMPILER_API "gnu")
+
+set(cc arm-zephyr-eabi-gcc)
+if (DEFINED TOOLCHAIN_CC_ARM_ZEPHYR_EABI)
+  set(cc ${TOOLCHAIN_CC_ARM_ZEPHYR_EABI})
+endif()
+set(cxx arm-zephyr-eabi-g++)
+if (DEFINED TOOLCHAIN_CXX_ARM_ZEPHYR_EABI)
+  set(cxx ${TOOLCHAIN_CXX_ARM_ZEPHYR_EABI})
+endif()
+
+find_program(COMPILER_BINARY ${cc}
+  PATH_SUFFIXES "bin"
+  HINTS ENV TOOLCHAIN_ROOT_ARM_ZEPHYR_EABI
+  REQUIRED)
+get_filename_component(TOOLCHAIN_ROOT ${COMPILER_BINARY} DIRECTORY)
+
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_ROOT}/arm-zephyr-eabi-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_ROOT}/arm-zephyr-eabi-g++)
+set(OBJCOPY            ${TOOLCHAIN_ROOT}/arm-zephyr-eabi-objcopy)
+set(OBJDUMP            ${TOOLCHAIN_ROOT}/arm-zephyr-eabi-objdump)
+set(SIZE               ${TOOLCHAIN_ROOT}/arm-zephyr-eabi-size)
+
+set(CMAKE_C_FLAGS_INIT
+    "-fdata-sections -ffunction-sections -frecord-gcc-switches -pipe")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
