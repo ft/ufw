e92df9c877daabf6ae468450f518e6c2a38d262e
Frank Terbeck <ft@bewatermyfriend.org>
2024-02-20 23:02:41 +0100

    Add mips toolchains file and qemu-based test runner
    On debian, you can use this:
    
      apt install gcc-mips-linux-gnu gcc-multilib-mips-linux-gnu
      apt install g++-mips-linux-gnu g++-multilib-mips-linux-gnu
    
    And add something like this to your mmh configuration:
    
        toolchains:
          - name: mips-linux-gnu
            architecture: [ mips ]
    
    This builds mmh for a big-endian mips target with programs as linux style
    executables and runs its test-suite through qemu. Interestingly right now:
    
        code-under-test/test/t-binary-format ............. ok
        code-under-test/test/t-convolution-low-pass ...... ok
        code-under-test/test/t-hexdump ................... ok
        code-under-test/test/t-length-prefix ............. ok
        code-under-test/test/t-persistent-storage ........ ok
        code-under-test/test/t-register-protocol ......... fail
        code-under-test/test/t-register-table ............ fail
        code-under-test/test/t-register-table-persistent-storage  ok
        code-under-test/test/t-rfc1055 ................... ok
        code-under-test/test/t-ring-buffer ............... ok
        code-under-test/test/t-sx-parser ................. ok
        code-under-test/test/t-varint .................... ok
    
    We'll fix this later, it is out of scope for this commit.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ cmake/toolchains/mips-linux-gnu.cmake
@@ -0,0 +1,31 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR mips)
+
+set(TOOLCHAIN_ID "gcc-mips")
+set(COMPILER_API "gnu")
+
+set(cc mips-linux-gnu-gcc)
+if (DEFINED TOOLCHAIN_CC_GNU_MIPS_LINUX_GNU)
+  set(cc ${TOOLCHAIN_CC_GNU_MIPS_LINUX_GNU})
+endif()
+set(cxx mips-linux-gnu-g++)
+if (DEFINED TOOLCHAIN_CXX_GNU_MIPS_LINUX_GNU)
+  set(cxx ${TOOLCHAIN_CXX_GNU_MIPS_LINUX_GNU})
+endif()
+
+find_program(COMPILER_BINARY ${cc}
+  PATH_SUFFIXES "bin"
+  HINTS ENV TOOLCHAIN_ROOT_GNU_MIPS_LINUX_GNU
+  REQUIRED)
+get_filename_component(TOOLCHAIN_ROOT ${COMPILER_BINARY} DIRECTORY)
+
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_ROOT}/mips-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_ROOT}/mips-linux-gnu-g++)
+set(OBJCOPY            ${TOOLCHAIN_ROOT}/mips-linux-gnu-objcopy)
+set(OBJDUMP            ${TOOLCHAIN_ROOT}/mips-linux-gnu-objdump)
+set(SIZE               ${TOOLCHAIN_ROOT}/mips-linux-gnu-size)
+
+set(CMAKE_C_FLAGS_INIT
+    "-static -fdata-sections -ffunction-sections -frecord-gcc-switches -pipe")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
