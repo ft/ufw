83800df3b043e9df919b5a4b06f985fa469a000d
Frank Terbeck <ft@bewatermyfriend.org>
2024-03-04 14:43:24 +0100

    cmake: Remove workaround for recursive cmake calls
    This should have been removed when the rest was removed before v2.0.0 was
    released in 2022.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- cmake/modules/UFWCompiler.cmake
+++ cmake/modules/UFWCompiler.cmake
@@ -4,17 +4,6 @@ endif()
 set(__UFW_UFWCompiler 1)
 
 include(CheckCSourceCompiles)
-
-function(ufw_compiler_has_type type var)
-  # check_c_source_compiles() uses try_compile, which will use the toolchain
-  # file we used in a build again. Unfortunately, it doesn't hand in any
-  # parameters that were passed to the initial cmake call. Do when we're
-  # adjusting CGT_TOOLCHAIN_ROOT for the TI toolchains on the command line,
-  # that is not picked up by sub-build in try_compile(), which leads to the
-  # compiler not finding its header files. This all seems to brittle. :-(
-  if (DEFINED CGT_TOOLCHAIN_ROOT)
-    set(CMAKE_REQUIRED_INCLUDES "${CGT_TOOLCHAIN_ROOT}/include")
-  endif()
   check_c_source_compiles("
 #include <stdint.h>
 #include <stddef.h>
