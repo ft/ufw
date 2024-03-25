9ad35074621496a4a4921ba217f6750f6acb3a92
Frank Terbeck <ft@bewatermyfriend.org>
2024-03-25 04:04:03 +0100

    lenp: Add fixed width length prefix framing
    This extends the variable width prefix encoding by fixed size variants. There
    is support for 8, 16, and 32 bit wide integers in either big or little endian
    byte order.
    
    The extended system uses flenp_ as its prefix. The lenp_ system is still in
    place for backward compatibility. The old system still passes the test suite.
    
    Should still add some tests for the fixed width variants.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- include/ufw/length-prefix.h
+++ include/ufw/length-prefix.h
@@ -18,6 +18,15 @@
 #include <ufw/endpoints.h>
 #include <ufw/variable-length-integer.h>
 
+typedef enum ufw_lenp_kind {
+    LENP_VARIABLE,
+    LENP_OCTET,
+    LENP_LE_16BIT,
+    LENP_LE_32BIT,
+    LENP_BE_16BIT,
+    LENP_BE_32BIT
+} LengthPrefixKind;
+
 typedef struct ufw_length_prefix_buffer {
     unsigned char prefix_[VARINT_64BIT_MAX_OCTETS];
     ByteBuffer prefix;
@@ -30,19 +39,91 @@ typedef struct ufw_length_prefix_chunks {
     ByteChunks payload;
 } LengthPrefixChunks;
 
-int lenp_memory_encode(LengthPrefixBuffer*, void*, size_t);
-int lenp_buffer_encode(LengthPrefixBuffer*, ByteBuffer*);
-int lenp_buffer_encode_n(LengthPrefixBuffer*, ByteBuffer*, size_t);
-int lenp_chunks_use(LengthPrefixChunks*);
+int flenp_memory_encode(LengthPrefixKind, LengthPrefixBuffer*, void*, size_t);
+int flenp_buffer_encode(LengthPrefixKind, LengthPrefixBuffer*, ByteBuffer*);
+int flenp_buffer_encode_n(LengthPrefixKind, LengthPrefixBuffer*,
+                          ByteBuffer*, size_t);
+int flenp_chunks_use(LengthPrefixKind, LengthPrefixChunks*);
 
-ssize_t lenp_memory_to_sink(Sink*, void*, size_t);
-ssize_t lenp_buffer_to_sink(Sink*, ByteBuffer*);
-ssize_t lenp_buffer_to_sink_n(Sink*, ByteBuffer*, size_t);
-ssize_t lenp_chunks_to_sink(Sink*, ByteChunks*);
+ssize_t flenp_memory_to_sink(LengthPrefixKind, Sink*, void*, size_t);
+ssize_t flenp_buffer_to_sink(LengthPrefixKind, Sink*, ByteBuffer*);
+ssize_t flenp_buffer_to_sink_n(LengthPrefixKind, Sink*, ByteBuffer*, size_t);
+ssize_t flenp_chunks_to_sink(LengthPrefixKind, Sink*, ByteChunks*);
 
-ssize_t lenp_memory_from_source(Source*, void*, size_t);
-ssize_t lenp_buffer_from_source(Source*, ByteBuffer*);
+ssize_t flenp_memory_from_source(LengthPrefixKind, Source*, void*, size_t);
+ssize_t flenp_buffer_from_source(LengthPrefixKind, Source*, ByteBuffer*);
 
-ssize_t lenp_decode_source_to_sink(Source*, Sink*);
+ssize_t flenp_decode_source_to_sink(LengthPrefixKind, Source*, Sink*);
+
+/*
+ * For backward compatibility, we implement the lenp_* functions in terms of
+ * flenp_* with  kind set to LENP_VARIABLE.
+ */
+
+static inline int
+lenp_memory_encode(LengthPrefixBuffer *lpb, void *buf, size_t n)
+{
+    return flenp_memory_encode(LENP_VARIABLE, lpb, buf, n);
+}
+
+static inline int
+lenp_buffer_encode(LengthPrefixBuffer *lpb, ByteBuffer *bb)
+{
+    return flenp_buffer_encode(LENP_VARIABLE, lpb, bb);
+}
+
+static inline int
+lenp_buffer_encode_n(LengthPrefixBuffer *lpb, ByteBuffer *bb, size_t n)
+{
+    return flenp_buffer_encode_n(LENP_VARIABLE, lpb, bb, n);
+}
+
+static inline int
+lenp_chunks_use(LengthPrefixChunks *lpc)
+{
+    return flenp_chunks_use(LENP_VARIABLE, lpc);
+}
+
+static inline ssize_t
+lenp_memory_to_sink(Sink *sink, void *buf, size_t n)
+{
+    return flenp_memory_to_sink(LENP_VARIABLE, sink, buf, n);
+}
+
+static inline ssize_t
+lenp_buffer_to_sink(Sink *sink, ByteBuffer *bb)
+{
+    return flenp_buffer_to_sink(LENP_VARIABLE, sink, bb);
+}
+
+static inline ssize_t
+lenp_buffer_to_sink_n(Sink *sink, ByteBuffer *bb, size_t n)
+{
+    return flenp_buffer_to_sink_n(LENP_VARIABLE, sink, bb, n);
+}
+
+static inline ssize_t
+lenp_chunks_to_sink(Sink *sink, ByteChunks *bc)
+{
+    return flenp_chunks_to_sink(LENP_VARIABLE, sink, bc);
+}
+
+static inline ssize_t
+lenp_memory_from_source(Source *source, void *buf, size_t n)
+{
+    return flenp_memory_from_source(LENP_VARIABLE, source, buf, n);
+}
+
+static inline ssize_t
+lenp_buffer_from_source(Source *source, ByteBuffer *bb)
+{
+    return flenp_buffer_from_source(LENP_VARIABLE, source, bb);
+}
+
+static inline ssize_t
+lenp_decode_source_to_sink(Source *source, Sink *sink)
+{
+    return flenp_decode_source_to_sink(LENP_VARIABLE, source, sink);
+}
 
 #endif /* INC_UFW_LENGTH_PREFIX_H */
--- src/length-prefix.c
+++ src/length-prefix.c
@@ -8,12 +8,15 @@
  * @file length-prefix.c
  * @brief Length prefix framing implementation
  *
- * This implements 64-bit variable-length-integer length prefix framing.
+ * This module implements two length prefix encoding schemes: One based on
+ * variable-length integers as specified in Google's protobuf serialisation
+ * format. The other is a family of encodings based on different fixed word
+ * width integer encodings.
  *
- * For unreliable data channels, RFC1055 (SLIP) implements a robust,
+ * Rationale: For unreliable data channels, RFC1055 (SLIP) implements a robust,
  * automatically re-synchronising kind of framing. SLIP however has two large
  * downsides: A large worst case overhead, and the inability to make use of
- * techniques such as DMA.
+ * techniques such as DMA. For a SLIP implementation see the rfc1055.h module.
  *
  * For reliable channels, such as TCP, length prefixing immediately fixes the
  * latter, because it does not transform the its payload. For naïve integer
@@ -37,6 +40,20 @@
  * This implements two kinds of encoders: One that directly sends into a sink
  * and another, that keeps the encoded variant inside of a data type, in case
  * the result is required multiple times.
+ *
+ * The previously described API uses variable length integers, combining low
+ * overhead with small frames while allowing for arbitrarily large frames as
+ * well. While this is a rather elegant solution, there are systems that use a
+ * fixed number of octets to perform a similar job. These systems use different
+ * kinds of octet orders and word sizes in their encoding. We can use the API
+ * from binary-format.h to deal with that, but it leads to a whole family of
+ * different possible implementations.
+ *
+ * This module also implements this family of encodings for 8, 16, and 32 bits,
+ * with little and big endian octet orders. Other word widths are possible of
+ * course, but these two seem most applicable to embedded systems. So we will
+ * stick with these for now. The code is written in a style that makes is easy
+ * to extend these encoding kinds later.
  */
 
 #include <stddef.h>
@@ -45,13 +62,79 @@
 #include <ufw/compat/errno.h>
 #include <ufw/compat/ssize-t.h>
 
+#include <ufw/binary-format.h>
 #include <ufw/byte-buffer.h>
 #include <ufw/endpoints.h>
 #include <ufw/length-prefix.h>
 #include <ufw/variable-length-integer.h>
 
+typedef unsigned char (*octet_parse)(const void*);
+typedef void* (*octet_generate)(void*, const unsigned char);
+
+static unsigned char
+ref_u8(const void *buf)
+{
+    const unsigned char *data = buf;
+    return *data;
+}
+
+static void*
+set_u8(void *buf, const unsigned char value)
+{
+    unsigned char *data = buf;
+    *data = value;
+    return data + 1u;
+}
+
+typedef uint16_t (*u16_parse)(const void*);
+typedef uint32_t (*u32_parse)(const void*);
+typedef void* (*u16_generate)(void*, const uint16_t);
+typedef void* (*u32_generate)(void*, const uint32_t);
+
+struct u8serdes {
+    octet_parse parse;
+    octet_generate generate;
+};
+
+struct u16serdes {
+    u16_parse parse;
+    u16_generate generate;
+};
+
+struct u32serdes {
+    u32_parse parse;
+    u32_generate generate;
+};
+
+struct flenp_serdes {
+    unsigned int size;
+    union {
+        struct u8serdes u8;
+        struct u16serdes u16;
+        struct u32serdes u32;
+    } cb;
+    uint64_t maximum;
+};
+
+#define KIND(K,S,T,P,G,M)      \
+    [LENP_ ## K] = {           \
+        .size = S,             \
+        .cb.T.parse = P,       \
+        .cb.T.generate = G,    \
+        .maximum = M }
+
+static struct flenp_serdes kind[] = {
+    KIND(VARIABLE, 0u,  u8, NULL,        NULL,        0u),
+    KIND(OCTET,    1u,  u8, ref_u8,      set_u8,      255u),
+    KIND(LE_16BIT, 2u, u16, bf_ref_u16l, bf_set_u16l, UINT16_MAX),
+    KIND(LE_32BIT, 4u, u32, bf_ref_u32l, bf_set_u32l, UINT32_MAX),
+    KIND(BE_16BIT, 2u, u16, bf_ref_u16b, bf_set_u16b, UINT16_MAX),
+    KIND(BE_32BIT, 4u, u32, bf_ref_u32b, bf_set_u32b, UINT32_MAX)
+};
+
 static int
-encode_prefix(ByteBuffer *b, unsigned char *mem, size_t n)
+encode_prefix(const LengthPrefixKind k, ByteBuffer *b,
+              unsigned char *mem, size_t n)
 {
     if (n > SSIZE_MAX) {
         return -EINVAL;
@@ -61,16 +144,28 @@ encode_prefix(ByteBuffer *b, unsigned char *mem, size_t n)
         return -EINVAL;
     }
 #endif
-    byte_buffer_space(b, mem, VARINT_64BIT_MAX_OCTETS);
-    /* The type definition ensures, this will not return an error. */
-    (void)varint_encode_u64(b, n);
+    if (k != LENP_VARIABLE && n > kind[k].maximum) {
+        return -EINVAL;
+    }
+    if (k == LENP_VARIABLE) {
+        byte_buffer_space(b, mem, VARINT_64BIT_MAX_OCTETS);
+    } else {
+        byte_buffer_use(b, mem, kind[k].size);
+    }
+    switch (kind[k].size) {
+    case 1:  kind[k].cb.u8.generate( mem, n); break;
+    case 2:  kind[k].cb.u16.generate(mem, n); break;
+    case 4:  kind[k].cb.u32.generate(mem, n); break;
+    default: varint_encode_u64(b, n);         break;
+    }
     return 0;
 }
 
 int
-lenp_memory_encode(LengthPrefixBuffer *lpb, void *buf, size_t n)
+flenp_memory_encode(const LengthPrefixKind k, LengthPrefixBuffer *lpb,
+                    void *buf, size_t n)
 {
-    const int rc = encode_prefix(&lpb->prefix, lpb->prefix_, n);
+    const int rc = encode_prefix(k, &lpb->prefix, lpb->prefix_, n);
 
     if (rc < 0) {
         return rc;
@@ -80,33 +175,36 @@ lenp_memory_encode(LengthPrefixBuffer *lpb, void *buf, size_t n)
 }
 
 int
-lenp_buffer_encode(LengthPrefixBuffer *lpb, ByteBuffer *b)
+flenp_buffer_encode(const LengthPrefixKind k,
+                    LengthPrefixBuffer *lpb, ByteBuffer *b)
 {
     const size_t rest = byte_buffer_rest(b);
-    return lenp_memory_encode(lpb, b->data + b->offset, rest);
+    return flenp_memory_encode(k, lpb, b->data + b->offset, rest);
 }
 
+
 int
-lenp_buffer_encode_n(LengthPrefixBuffer *lpb, ByteBuffer *b, size_t n)
+flenp_buffer_encode_n(const LengthPrefixKind k,
+                      LengthPrefixBuffer *lpb, ByteBuffer *b, size_t n)
 {
     const size_t rest = byte_buffer_rest(b);
     if (n > rest) {
         return -EINVAL;
     }
-    const int rc = lenp_memory_encode(lpb, b->data + b->offset, n);
+    const int rc = flenp_memory_encode(k, lpb, b->data + b->offset, n);
     b->offset += rest;
     return rc;
 }
 
 int
-lenp_chunks_use(LengthPrefixChunks *lpc)
+flenp_chunks_use(const LengthPrefixKind k, LengthPrefixChunks *lpc)
 {
     size_t size = 0u;
     for (size_t i = lpc->payload.active; i < lpc->payload.chunks; ++i) {
         size += byte_buffer_rest(lpc->payload.chunk + i);
     }
 
-    const int rc = encode_prefix(&lpc->prefix, lpc->prefix_, size);
+    const int rc = encode_prefix(k, &lpc->prefix, lpc->prefix_, size);
 
     if (rc < 0) {
         return rc;
@@ -116,17 +214,16 @@ lenp_chunks_use(LengthPrefixChunks *lpc)
 }
 
 ssize_t
-lenp_memory_to_sink(Sink *sink, void *buf, size_t n)
+flenp_memory_to_sink(const LengthPrefixKind k, Sink *sink, void *buf, size_t n)
 {
-    if (n > SSIZE_MAX) {
-        return -EINVAL;
+    LengthPrefixBuffer lpb;
+    const int rc = encode_prefix(k, &lpb.prefix, lpb.prefix_, n);
+
+    if (rc < 0) {
+        return rc;
     }
-#if SSIZE_MAX > UINT64_MAX
-    if (n > UINT64_MAX) {
-        return -EINVAL;
-    }
-#endif
-    const size_t numlen = varint_u64_length((uint64_t) n);
+
+    const size_t numlen = lpb.prefix.used;
     if (n > (SSIZE_MAX - numlen)) {
         return -EINVAL;
     }
@@ -135,62 +232,62 @@ lenp_memory_to_sink(Sink *sink, void *buf, size_t n)
         return -EINVAL;
     }
 #endif
-    const ssize_t rv = numlen + n;
+
     {
-        const int rc = varint_u64_to_sink(sink, (uint64_t)n);
-        if (rc < 0) {
-            return (ssize_t)rc;
+        const int rcsink = sink_put_chunk(sink, lpb.prefix_, numlen);
+        if (rcsink < 0) {
+            return (ssize_t)rcsink;
         }
     }
 
     {
-        const ssize_t rc = sink_put_chunk(sink, buf, n);
-        if (rc < 0) {
-            return rc;
-        } else if (rc == 0) {
+        const ssize_t rcsink = sink_put_chunk(sink, buf, n);
+        if (rcsink < 0) {
+            return rcsink;
+        } else if (rcsink == 0) {
             return 0;
         }
     }
 
-    return rv;
+    return (ssize_t)(numlen + n);
 }
 
 ssize_t
-lenp_buffer_to_sink(Sink *sink, ByteBuffer *b)
+flenp_buffer_to_sink(const LengthPrefixKind k, Sink *sink, ByteBuffer *b)
 {
-    return lenp_memory_to_sink(sink, b->data + b->offset, byte_buffer_avail(b));
+    return flenp_memory_to_sink(k, sink, b->data + b->offset,
+                                byte_buffer_avail(b));
 }
 
 ssize_t
-lenp_buffer_to_sink_n(Sink *sink, ByteBuffer *b, size_t n)
+flenp_buffer_to_sink_n(const LengthPrefixKind k,
+                       Sink *sink, ByteBuffer *b, size_t n)
 {
     const size_t rest = byte_buffer_avail(b);
     if (n > rest) {
         return -EINVAL;
     }
-    const int rc = lenp_memory_to_sink(sink, b->data + b->offset, rest);
+    const int rc = flenp_memory_to_sink(k, sink, b->data + b->offset, rest);
     b->offset += rest;
     return rc;
 }
 
 ssize_t
-lenp_chunks_to_sink(Sink *sink, ByteChunks *oc)
+flenp_chunks_to_sink(const LengthPrefixKind k, Sink *sink, ByteChunks *oc)
 {
     size_t size = 0u;
     for (size_t i = oc->active; i < oc->chunks; ++i) {
         size += byte_buffer_rest(oc->chunk + i);
     }
 
-    if (size > SSIZE_MAX) {
-        return -EINVAL;
-    }
-#if SSIZE_MAX > UINT64_MAX
-    if (size > UINT64_MAX) {
-        return -EINVAL;
-    }
-#endif
+    LengthPrefixBuffer lpb;
+    const int rc = encode_prefix(k, &lpb.prefix, lpb.prefix_, size);
 
-    const size_t numlen = varint_u64_length((uint64_t)size);
+    if (rc < 0) {
+        return rc;
+    }
+
+    const size_t numlen = lpb.prefix.used;
     if (size > (SSIZE_MAX - numlen)) {
         return -EINVAL;
     }
@@ -199,36 +296,63 @@ lenp_chunks_to_sink(Sink *sink, ByteChunks *oc)
         return -EINVAL;
     }
 #endif
-    const ssize_t rv = numlen + size;
+
     {
-        const int rc = varint_u64_to_sink(sink, (uint64_t)size);
-        if (rc < 0) {
-            return (ssize_t)rc;
+        const int rcsink = sink_put_chunk(sink, lpb.prefix_, numlen);
+        if (rcsink < 0) {
+            return (ssize_t)rcsink;
         }
     }
 
     for (size_t i = oc->active; i < oc->chunks; ++i) {
         const size_t n = byte_buffer_rest(oc->chunk + i);
-        const ssize_t rc = sink_put_chunk(
+        const ssize_t rcsink = sink_put_chunk(
             sink, oc->chunk[i].data + oc->chunk[i].offset, n);
-        if (rc < 0) {
-            return rc;
-        } else if (rc == 0) {
+        if (rcsink < 0) {
+            return rcsink;
+        } else if (rcsink == 0) {
             return 0;
         }
     }
 
-    return rv;
+    return (ssize_t)(numlen + size);
+}
+
+static ssize_t
+decode_prefix(const LengthPrefixKind k, Source *source, uint64_t *len)
+{
+    if (k == LENP_VARIABLE) {
+        return varint_u64_from_source(source, len);
+    }
+
+    unsigned char buf[4];
+    const unsigned int n = kind[k].size;
+    {
+        const ssize_t rcsrc = source_get_chunk(source, buf, n);
+        if (rcsrc < 0) {
+            return rcsrc;
+        }
+    }
+
+    switch (kind[k].size) {
+    case 1: *len = kind[k].cb.u8.parse( buf); break;
+    case 2: *len = kind[k].cb.u16.parse(buf); break;
+    case 4: *len = kind[k].cb.u32.parse(buf); break;
+    default: return -EINVAL;
+    }
+
+    return 0;
 }
 
 ssize_t
-lenp_memory_from_source(Source *source, void *mem, size_t size)
+flenp_memory_from_source(const LengthPrefixKind k,
+                         Source *source, void *mem, size_t size)
 {
     uint64_t len = 0u;
     {
-        const int rc = varint_u64_from_source(source, &len);
+        const ssize_t rc = decode_prefix(k, source, &len);
         if (rc < 0) {
-            return (ssize_t)rc;
+            return rc;
         }
     }
     if (len > size) {
@@ -238,11 +362,12 @@ lenp_memory_from_source(Source *source, void *mem, size_t size)
 }
 
 ssize_t
-lenp_buffer_from_source(Source *source, ByteBuffer *b)
+flenp_buffer_from_source(const LengthPrefixKind k,
+                         Source *source, ByteBuffer *b)
 {
     const ssize_t rc =
-        lenp_memory_from_source(source, b->data + b->offset,
-                                byte_buffer_avail(b));
+        flenp_memory_from_source(k, source, b->data + b->offset,
+                                 byte_buffer_avail(b));
 
     if (rc >= 0) {
         b->offset += rc;
@@ -252,13 +377,14 @@ lenp_buffer_from_source(Source *source, ByteBuffer *b)
 }
 
 ssize_t
-lenp_decode_source_to_sink(Source *source, Sink *sink)
+flenp_decode_source_to_sink(const LengthPrefixKind k,
+                            Source *source, Sink *sink)
 {
     uint64_t len = 0u;
     {
-        const int rc = varint_u64_from_source(source, &len);
+        const ssize_t rc = decode_prefix(k, source, &len);
         if (rc < 0) {
-            return (ssize_t)rc;
+            return rc;
         }
     }
 
