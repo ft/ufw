14c5741105774a8601fc0b4e5e80f82e8057deb7
Frank Terbeck <ft@bewatermyfriend.org>
2024-05-28 23:46:57 +0200

    Housekeeping for release
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- include/ufw/meta.h
+++ include/ufw/meta.h
@@ -16,7 +16,7 @@
 #define UFW_LIBRARY_MAJOR 5u
 
 /** Micro Framework Library Minor Version Number */
-#define UFW_LIBRARY_MINOR 5u
+#define UFW_LIBRARY_MINOR 6u
 
 /** Micro Framework Library Patchlevel */
 #define UFW_LIBRARY_PATCH 0u
