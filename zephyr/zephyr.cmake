6b7ccd3c3ae2f3ecaa61d984e633553674aaafc3
Frank Terbeck <ft@bewatermyfriend.org>
2024-11-10 00:30:59 +0100

    zephyr: Inherit compiler setup in set_target_cpu
    This way, any module that uses ufw's cmake stuff to setup their build flags
    will inherit from zephyr as well.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- zephyr/zephyr.cmake
+++
@@ -1,5 +0,0 @@
-# This makes ufw inherit the toolchain setup of the Zephyr build system.
-foreach (_lib ${__ufw_targets})
-  message(STATUS "ufw,zephyr: Toolchain setup for ${_lib}")
-  target_link_libraries("${_lib}" PRIVATE zephyr_interface)
-endforeach()
