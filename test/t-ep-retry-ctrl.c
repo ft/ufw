57e04d6a0006a4f02d54e1de5ed491c00c65124c
Frank Terbeck <ft@bewatermyfriend.org>
2024-11-12 22:05:30 +0100

    ep: Add retry control test stub
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ test/t-ep-retry-ctrl.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <ufw/compat/errno.h>
+
+#include <ufw/compiler.h>
+#include <ufw/endpoints.h>
+
+#include <ufw/test/tap.h>
+
+int
+main(UNUSED int argc, UNUSED char **argv)
+{
+    plan(0);
+    return EXIT_SUCCESS;
+}
