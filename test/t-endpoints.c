d9dd93a1c8d16a56fe4e37cffbdc52b59f6ff381
Frank Terbeck <ft@bewatermyfriend.org>
2024-11-13 22:15:24 +0100

    ep: Add endpoints test stub
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ test/t-endpoints.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <ufw/compiler.h>
+#include <ufw/endpoints.h>
+
+#include <ufw/test/tap.h>
+
+int
+main(UNUSED int argc, UNUSED char **argv)
+{
+    plan(0);
+    return EXIT_SUCCESS;
+}
