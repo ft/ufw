58f36088c8f06d52090cf6adc65712dc1331875d
Frank Terbeck <ft@bewatermyfriend.org>
2025-01-22 15:55:24 +0100

    wip

---
+++ include/ufw/versioned-persistence.h
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2025 ufw workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+/**
+ * @file versioned-persistence.h
+ * @brief Persistence storage with version information
+ */
+
+#ifndef INC_UFW_VERSIONED_PERSISTENCE_H_91da0966
+#define INC_UFW_VERSIONED_PERSISTENCE_H_91da0966
+
+#include <ufw/bit-operations.h>
+#include <ufw/byte-buffer.h>
+#include <ufw/endpoints.h>
+#include <ufw/persistent-storage.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+struct vp_access {
+    Source fetch;
+    Sink store;
+};
+
+struct vp_meta {
+    uint32_t address;
+    uint16_t size;
+    uint16_t version;
+};
+
+struct vp_checksum {
+    PersistentChecksumType type;
+    PersistentChecksum initial;
+    union {
+        PersistentChksum16 c16;
+        PersistentChksum32 c32;
+    } process;
+};
+
+#define VP_STATE_CONSISTENT BIT(0)
+#define VP_STATE_COMPATIBLE BIT(1)
+
+typedef struct versioned_persistence {
+    uint16_t state;
+    struct vp_meta meta;
+    struct vp_access data;
+    struct vp_checksum chksum;
+    ByteBuffer *buffer;
+} VersionedPersistence;
+
+#define VP16_INIT(ADDR, SIZE, VERSION, FETCH, STORE, CHKSUM, INIT)  \
+    {   .state = 0u,                                                \
+        .meta.address = (ADDR),                                     \
+        .meta.size = (SIZE),                                        \
+        .meta.version = (VERSION),                                  \
+        .data.fetch = (FETCH),                                      \
+        .data.store = (STORE),                                      \
+        .chksum.type = PERSISTENT_CHECKSUM_16BIT,                   \
+        .chksum.initial.sum16 = (INIT),                             \
+        .chksum.process.c16 = (CHKSUM),                             \
+        .buffer = NULL }
+
+#if 0
+/**
+ * Variant of PersistentStorage that adds content versioning
+ *
+ * PersistentStorage offers a low-level abstraction, that allows to store data
+ * in persistent memory, so it can be accessed across many system starts, while
+ * ensuring that the data retrieved is consistent with regard to a checksum
+ * algorithm.
+ *
+ * What PersistentStorage does not allow is to tell if the data stored is
+ * compatible with the active application: The software accessing the
+ * persistent memory may change in the course of time. From version to version,
+ * data that needs to be stored across system starts may change content and
+ * semantics. Extending PersistentStorage with versioning information is this
+ * type's job.
+ *
+ * In persistent memory, this can be thought of as this:
+ *
+ * @code
+ * struct {
+ *     Checksum chksum;   // 16 or 32 bits.
+ *     uint16_t size;     // Size of data section.
+ *     uint16_t version;  // Number identifying the version of data section.
+ *     unsigned char data[size];
+ * };
+ * @endcode
+ *
+ * This type is implemented on top of PersistentStorage, and can be accessed
+ * through it, even if compatibility cannot be ensured, but consistency can.
+ *
+ * Consistency is established by PersistentStorage. Compatibility is assumed if
+ * `version.expected` equals `version.stored`.
+ */
+typedef struct versioned_persistence_old {
+    /** Underlying PersistentStorage instance */
+    PersistentStorage ps;
+    struct {
+        /** This is specifies the version the software is compatible with. */
+        uint16_t expected
+        /** This is the version retrieved from the persistent memory. */
+        uint16_t stored;
+    } version;
+    struct {
+        /** Calculated size of the data section in memory */
+        size_t size;
+        /**
+         * Offset of data section from the start of the memory the
+         * PersistentStorage instance accesses.
+         */
+        off_t offset;
+    } data;
+} VersionedPersistenceOld;
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* INC_UFW_VERSIONED_PERSISTENCE_H_91da0966 */
---
+++ test/t-versioned-persistence.c
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2025 ufw workers, All rights reserved.
+ *
+ * Terms for redistribution and use can be found in LICENCE.
+ */
+
+#include <asm-generic/errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <ufw/compiler.h>
+#include <ufw/test/tap.h>
+
+#include <ufw/crc/crc16-arc.h>
+#include <ufw/compat/ssize-t.h>
+#include <ufw/versioned-persistence.h>
+
+/*
+ * Test setup
+ */
+
+#define STORAGE_SIZE 1024
+
+#define INFO_ADDRESS 24u
+#define INFO_SIZE    32u
+#define INFO_VERSION  5u
+
+static unsigned char storage[STORAGE_SIZE];
+
+static void
+t_reset(void)
+{
+    memset(storage, UCHAR_MAX, STORAGE_SIZE);
+}
+
+/*
+ * CRC Adaptation. The standard crc API does not match PersistentChecksum.
+ * Maybe add more variants.
+ */
+
+static uint16_t
+crc16(const unsigned char *buffer, const size_t n, const uint16_t init)
+{
+    return ufw_crc16_arc(init, buffer, n);
+}
+
+/*
+ * Simulating a peripheral API
+ */
+
+static ssize_t
+buffer_read(uint32_t addr, void *buf, size_t n)
+{
+    if ((addr + n) > STORAGE_SIZE) {
+        return 0;
+        return -ENOBUFS;
+    }
+
+    memcpy(buf, storage + addr, n);
+    return n;
+}
+
+static ssize_t
+buffer_write(uint32_t addr, const void *buf, size_t n)
+{
+    if ((addr + n) > STORAGE_SIZE) {
+        return -ENOBUFS;
+    }
+
+    memcpy(storage + addr, buf, n);
+    return n;
+}
+
+/*
+ * Endpoints with addressable memory
+ */
+
+struct addressable_source {
+    uint32_t address;
+    ssize_t (*run)(uint32_t, void*, size_t);
+    void *data;
+};
+
+static ssize_t
+run_addressable_source(void *driver, void *buffer, const size_t n)
+{
+    struct addressable_source *cfg = driver;
+    return cfg->run(cfg->address, buffer, n);
+}
+
+struct addressable_sink {
+    uint32_t address;
+    ssize_t (*run)(uint32_t, const void*, size_t);
+    void *data;
+};
+
+static ssize_t
+run_addressable_sink(void *driver, const void *buffer, const size_t n)
+{
+    struct addressable_sink *cfg = driver;
+    return cfg->run(cfg->address, buffer, n);
+}
+
+/* Tests */
+
+int
+main(UNUSED int argc, UNUSED char *argv[])
+{
+    struct addressable_source src = { .address = 0u, .run = buffer_read, .data = NULL };
+    Source storage_fetch = CHUNK_SOURCE_INIT(run_addressable_source, &src);
+    struct addressable_sink snk = { .address = 0u, .run = buffer_write, .data = NULL };
+    Sink storage_store = CHUNK_SINK_INIT(run_addressable_sink, &snk);
+
+    VersionedPersistence vp =
+        VP16_INIT(INFO_ADDRESS, INFO_SIZE, INFO_VERSION,
+                  storage_fetch, storage_store,
+                  crc16, CRC16_ARC_INITIAL);
+
+
+
+    plan(1);
+
+    t_reset();
+    ok((1 + 2) == 3, "math works");
+
+    return EXIT_SUCCESS;
+}
