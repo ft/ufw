e36107a5780d5004099bc34faa1bfd069cae0d49
Frank Terbeck <ft@bewatermyfriend.org>
2025-02-24 13:33:01 +0100

    zephyr,uart,poll: Drain UART RX in a little helper thread
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- include/ufwz/endpoint-uart-poll.h
+++ include/ufwz/endpoint-uart-poll.h
@@ -10,10 +10,59 @@
 int ufwz_uart_octet_sink(void *driver, unsigned char value);
 int ufwz_uart_octet_source(void *driver, void *value);
 
-#define UFWZ_UART_POLL_SOURCE(DRIVER)                           \
-    OCTET_SOURCE_INIT(ufwz_uart_octet_source, (void*)(DRIVER))
+#define UFWZ_UART_POLL_SOURCE(name)                     \
+    OCTET_SOURCE_INIT(                                  \
+        ufwz_uart_octet_source,                         \
+        (void*)(&ufwz_uart_poll_thread_data_##name))
 
 #define UFWZ_UART_POLL_SINK(DRIVER)                             \
     OCTET_SINK_INIT(ufwz_uart_octet_sink, (void*)(DRIVER))
+
+struct ufwz_uart_poll_thread_data {
+    const struct device *ifc;
+    struct k_pipe *pipe;
+    struct k_sem *sem;
+    int32_t yieldtime;
+};
+
+void ufwz_uart_poll_thread_cb(void *data, void *a, void *b);
+
+#define UFWZ_UART_POLL_THREAD(name, ifc_, pipesize, yt)                 \
+    K_PIPE_DEFINE(ufwz_uart_poll_thread_pipe_##name, pipesize, 4);      \
+    static struct ufwz_uart_poll_thread_data                            \
+    ufwz_uart_poll_thread_data_##name = {                               \
+        .ifc = (ifc_),                                                  \
+        .pipe = &ufwz_uart_poll_thread_pipe_##name,                     \
+        .sem = NULL,                                                    \
+        .yieldtime = (yt)                                               \
+    };                                                                  \
+    K_THREAD_DEFINE(ufwz_uart_poll_thread_##name,                       \
+                    128,                                                \
+                    ufwz_uart_poll_thread_cb,                           \
+                    &ufwz_uart_poll_thread_data_##name,                 \
+                    NULL, NULL,                                         \
+                    14, 0, 0)
+
+#define UFWZ_UART_POLL_THREAD_DELAYABLE(name, ifc_, pipesize, yt)       \
+    K_SEM_DEFINE(ufwz_uart_poll_thread_sem_##name, 0, 1);               \
+    K_PIPE_DEFINE(ufwz_uart_poll_thread_pipe_##name, pipesize, 4);      \
+    static struct ufwz_uart_poll_thread_data                            \
+    ufwz_uart_poll_thread_data_##name = {                               \
+        .ifc = (ifc_),                                                  \
+        .pipe = &ufwz_uart_poll_thread_pipe_##name,                     \
+        .sem = &ufwz_uart_poll_thread_sem_##name,                       \
+        .yieldtime = (yt)                                               \
+    };                                                                  \
+    K_THREAD_DEFINE(ufwz_uart_poll_thread_##name,                       \
+                    128,                                                \
+                    ufwz_uart_poll_thread_cb,                           \
+                    &ufwz_uart_poll_thread_data_##name,                 \
+                    NULL, NULL,                                         \
+                    14, 0, 0)
+
+#define UFWZ_UART_POLL_THREAD_START(name)               \
+    do {                                                \
+        k_sem_give(&ufwz_uart_poll_thread_sem_##name);  \
+    } while (0)
 
 #endif /* INC_INCLUDE_UFWZ_ENDPOINT_UART_POLL_H_0c903589 */
--- src/z/endpoint-uart-poll.c
+++ src/z/endpoint-uart-poll.c
@@ -4,20 +4,64 @@
  * Terms for redistribution and use can be found in LICENCE.
  */
 
+#include <zephyr/kernel.h>
+
 #include <zephyr/drivers/uart.h>
 
+#include <ufw/compiler.h>
 #include <ufwz/endpoint-uart-poll.h>
 
-int
-ufwz_uart_octet_source(void *driver, void *value)
-{
-    const int rc = uart_poll_in(driver, value);
-    return rc < 0 ? -EAGAIN : 1;
-}
+/* For transmissions in polling mode, we just use uart_poll_out without any
+ * further wrapping. */
 
 int
 ufwz_uart_octet_sink(void *driver, unsigned char value)
 {
     uart_poll_out(driver, value);
     return 1;
+}
+
+/*
+ * Receiving in poll mode sucks a little. Normally, it would be much more
+ * advisable to use the FIFO API for UARTs. Some however, like the one for
+ * native-sim, don't allow for that. So here we are. What we do is to run a
+ * small, low-priority thread, that polls for UART data and yields back to the
+ * scheduler when it can't read any data. This polling feeds a pipe, that a
+ * real processing thread can pend for data on.
+ */
+
+int
+ufwz_uart_octet_source(void *driver, void *value)
+{
+    struct ufwz_uart_poll_thread_data *data = driver;
+    struct k_pipe *p = data->pipe;
+    size_t done = 0u;
+    while (done == 0u) {
+        k_pipe_get(p, value, 1u, &done, 1u, K_FOREVER);
+    }
+    return 1;
+}
+
+void
+ufwz_uart_poll_thread_cb(void *data, UNUSED void *a, UNUSED void *b)
+{
+    struct ufwz_uart_poll_thread_data *cfg = data;
+
+    if (cfg->sem != NULL) {
+        k_sem_take(cfg->sem, K_FOREVER);
+    }
+    for (;;) {
+        unsigned char value = 0u;
+        while (uart_poll_in(cfg->ifc, &value) == 0) {
+            size_t done = 0;
+            while (done == 0) {
+                k_pipe_put(cfg->pipe, &value, 1u, &done, 1u, K_FOREVER);
+            }
+        }
+        if (cfg->yieldtime > 0) {
+            k_msleep(cfg->yieldtime);
+        } else {
+            k_yield();
+        }
+    }
 }
