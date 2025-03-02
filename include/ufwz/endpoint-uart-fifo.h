dbb6d5bad35c18710ba87d6d44bcf874026c5fbc
Frank Terbeck <ft@bewatermyfriend.org>
2025-03-02 22:41:14 +0100

    zephyr,uart,fifo: Allow this endpoint's source type to time out
    In error conditions, a communications partner may not respond. Being able to
    time out is vital in that case.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- include/ufwz/endpoint-uart-fifo.h
+++ include/ufwz/endpoint-uart-fifo.h
@@ -11,10 +11,23 @@
 
 #include <zephyr/device.h>
 
+struct ufwz_uart_fifo_data {
+    struct k_pipe *pipe;
+    k_timeout_t timeout;
+};
+
 int ufwz_uart_fifo_source(void *driver, void *value);
-int ufwz_uart_fifo_source_init(const struct device *dev, struct k_pipe *data);
+int ufwz_uart_fifo_source_init(const struct device *dev,
+                               struct ufwz_uart_fifo_data *data);
 
 #define UFWZ_UART_FIFO_SOURCE(DRIVER)                   \
     OCTET_SOURCE_INIT(ufwz_uart_fifo_source, DRIVER)
+
+#define UFWZ_DEFINE_UART_FIFO_DATA(name_, size_, timeout_)      \
+    K_PIPE_DEFINE(name_##_pipe, (size_), 4);                    \
+    struct ufwz_uart_fifo_data name_ = {                        \
+        .pipe = &(name_##_pipe),                                \
+        .timeout = (timeout_)                                   \
+    };
 
 #endif /* INC_INCLUDE_UFWZ_ENDPOINT_UART_FIFO_H_ade9b63d */
--- src/z/endpoint-uart-fifo.c
+++ src/z/endpoint-uart-fifo.c
@@ -14,7 +14,7 @@
 static void
 ufwz_uart_fifo_source_cb(const struct device *dev, void *user_data)
 {
-    struct k_pipe *pipe = user_data;
+    struct ufwz_uart_fifo_data *data = user_data;
     unsigned char buf[16];
 
     while (uart_irq_update(dev) && uart_irq_rx_ready(dev)) {
@@ -23,27 +23,35 @@ ufwz_uart_fifo_source_cb(const struct device *dev, void *user_data)
             continue;
         }
         size_t done;
-        k_pipe_put(pipe, buf, rc, &done, 1u, K_NO_WAIT);
+        k_pipe_put(data->pipe, buf, rc, &done, 1u, K_NO_WAIT);
     }
 }
 
 int
 ufwz_uart_fifo_source(void *driver, void *value)
 {
-    struct k_pipe *pipe = driver;
+    struct ufwz_uart_fifo_data *data = driver;
 
     size_t done = 0u;
     while (done == 0u) {
-        k_pipe_get(pipe, value, 1u, &done, 1u, K_FOREVER);
+        const int rc = k_pipe_get(data->pipe, value, 1u,
+                                  &done, 1u, data->timeout);
+        if (rc == -EAGAIN) {
+            /* k_pipe_get() uses this in case of timeout. Meh. */
+            return -ETIMEDOUT;
+        } else if (rc < 0) {
+            return rc;
+        }
     }
     return 1;
 }
 
 int
-ufwz_uart_fifo_source_init(const struct device *dev, struct k_pipe *pipe)
+ufwz_uart_fifo_source_init(const struct device *dev,
+                           struct ufwz_uart_fifo_data *data)
 {
     const int rc =
-        uart_irq_callback_user_data_set(dev, ufwz_uart_fifo_source_cb, pipe);
+        uart_irq_callback_user_data_set(dev, ufwz_uart_fifo_source_cb, data);
 
     if (rc < 0) {
         return 0;
