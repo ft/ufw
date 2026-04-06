ft/ufw

commit 4a01e4c0f258b69bd01a9f03f06664cde7cd2a18

Update copyright years
Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

tools/check-copyright.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2025 ufw workers, All rights reserved.
+# Copyright (c) 2025-2026 ufw workers, All rights reserved.
 #
 # Terms for redistribution and use can be found in LICENCE.
 
@@ -30,9 +30,9 @@ add) add=1
      mode=check
      ;;
 esac
-match='Copyright.*ufw workers.*reserved.'
-old_and_new='Copyright.*\(micro framework workers\|ufw\).*reserved.'
-old_and_new_perl='Copyright.*(micro framework workers|ufw).*reserved.'
+match='Copyright (c) 2025-2026 ufw workers, All rights reserved.'
+old_and_new='Copyright (c) 2025-2026 ufw workers, All rights reserved.'
+old_and_new_perl='Copyright (c) 2025-2026 ufw workers, All rights reserved.'
 
 # This function is called for each file name tracked by ufw's git repository.
 # For each file it gets two arguments: The directory name the file lives in and
@@ -132,9 +132,9 @@ make_notice () {
     from="$1"
     to="$2"
     if [ "$from" != "$to" ]; then
-        REPLY="Copyright (c) ${from}-${to} ufw workers, All rights reserved."
+        REPLY="Copyright (c) 2025-2026 ufw workers, All rights reserved."
     else
-        REPLY="Copyright (c) ${from} ufw workers, All rights reserved."
+        REPLY="Copyright (c) 2025-2026 ufw workers, All rights reserved."
     fi
 }
 

commit 98f9f1640180b22b2feaa8013d4fc06ee7ad3015

tools: Fix check-copyright.sh
This was broken in 4a01e4c0f25 and only partially fixed in 735ffe9a0. This
should be the rest. This also refrains from automagically updating files that
are to difficult to get right with simple patterns.

Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

tools/check-copyright.sh
@@ -132,9 +132,9 @@ make_notice () {
     from="$1"
     to="$2"
     if [ "$from" != "$to" ]; then
-        REPLY="Copyright (c) 2025-2026 ufw workers, All rights reserved."
+        REPLY="Copyright (c) ${from}-${to} ufw workers, All rights reserved."
     else
-        REPLY="Copyright (c) 2025-2026 ufw workers, All rights reserved."
+        REPLY="Copyright (c) ${from} ufw workers, All rights reserved."
     fi
 }
 
@@ -214,6 +214,10 @@ $REPLY"
     perl -i -pe 'print "'"$REPLY"'" if $. == '"$_offset" "$_file"
 }
 
+doityourself () {
+    printf 'check-copyright.sh: Not touching %s, do it manually!\n' "$1"
+}
+
 check_file () {
     _end="$1"
     _file="$2"
@@ -269,7 +273,11 @@ check_file () {
         make_notice "$_start" "$_end"
         new_notice="$REPLY"
         if [ "$update" -ne 0 ]; then
-            update_file "$_file" "$new_notice"
+            case "$_file" in
+            (tools/check-copyright.sh)      doityourself "$_file" ;;
+            (tools/make-binary-format.scm)  doityourself "$_file" ;;
+            (*) update_file "$_file" "$new_notice" ;;
+            esac
         fi
         _rc=1
     fi